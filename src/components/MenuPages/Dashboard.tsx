a0924e53cea64ca8bfb234de12e88bf20111bef1
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-08-07 13:49:56 +0700

    Add dashboard component

---
+++ src/components/MenuPages/Dashboard.tsx
@@ -0,0 +1,16 @@
+import Card from "@/components/Card";
+import { useEffect, useState } from "react";
+
+const DashboardContent = () => {
+  return (
+    <div>
+      <Card
+        title="Dashboard"
+        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam eligendi enim, quos soluta eveniet obcaecati? Ex iste aliquid in eveniet?"
+        imageSrc="https://images.unsplash.com/photo-1688045303217-f44fec8d3fe1?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+    </div>
+  );
+};
+
+export default DashboardContent;
