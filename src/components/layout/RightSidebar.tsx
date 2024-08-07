8161ae6e7340871b625bcd7e61a13cbd7465f4fe
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-08-07 13:49:46 +0700

    Add Right side bar component

---
+++ src/components/layout/RightSidebar.tsx
@@ -0,0 +1,10 @@
+const RightSidebar = () => {
+  return (
+    <div className="w-64 bg-accent text-white p-4">
+      {/* Sidebar content */}
+      <h2 className="text-xl">Right Sidebar</h2>
+    </div>
+  );
+};
+
+export default RightSidebar;
