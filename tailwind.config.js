a187821366566afd59689b81637b7af206d654d2
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-08-07 13:48:27 +0700

    Add color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    extend: {
+      colors: {
+        primary: "#f8b602",
+        secondary: "#ffffff",
+        accent: "#2e2e2e",
+        accent2: "#363636",
+      },
+    },
     fontFamily: {
       sans: ["Inter", "sans-serif"],
       thai: ["Pridi", "sans-serif"],
