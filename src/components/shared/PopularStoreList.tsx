bef6f35224b1d1465f64189ba89d129d8bf2f5d7
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-08-08 10:39:01 +0700

    Add carousel for store list

--- src/components/shared/PopularStoreList.tsx
+++ src/components/shared/PopularStoreList.tsx
@@ -1,8 +1,9 @@
 import StoreCard from "@/components/shared/StoreCard";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const PopularStoreList = () => {
-  // TODO: Replace cards array this with DB
   const cards = [
     {
       image:
@@ -44,17 +45,31 @@ const PopularStoreList = () => {
       maxSeats: 30,
       description: "This is a popular club where you can have fun.",
     },
-    {
-      image:
-        "https://northspace.life/wp-content/uploads/2024/01/hanguotcnx2024-04-1024x1024.jpg",
-      title: "MYST MAYA",
-      reservationStatus: "can't reserve",
-      rating: 5,
-      currSeats: 30,
-      maxSeats: 30,
-      description: "This is a popular club where you can have fun.",
-    },
   ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <div className="p-4">
@@ -66,9 +81,9 @@ const PopularStoreList = () => {
           </span>
         </div>
       </div>
-      <div className="flex items-center">
+      <Slider {...settings}>
         {cards.map((card, index) => (
-          <div key={index}>
+          <div key={index} className="px-1">
             <StoreCard
               image={card.image}
               title={card.title}
@@ -80,7 +95,7 @@ const PopularStoreList = () => {
             />
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
