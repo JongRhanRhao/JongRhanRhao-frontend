579715bef580044bed68898bd5808d34c1b78434
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-08-11 02:34:02 +0700

    Update

--- src/components/shared/StoreCard.tsx
+++ src/components/shared/StoreCard.tsx
@@ -1,16 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faHeart } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+// import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const StoreCard = ({
-  image,
-  title,
-  reservationStatus,
-  rating,
-  maxSeats,
-  currSeats,
-  description,
-}: {
+interface StoreCardProps {
+  id: number;
   image: string;
   title: string;
   reservationStatus: string;
@@ -18,52 +12,71 @@ const StoreCard = ({
   maxSeats: number;
   currSeats: number;
   description: string;
+}
+
+const StoreCard: React.FC<StoreCardProps> = ({
+  id,
+  image,
+  title,
+  reservationStatus,
+  rating,
+  maxSeats,
+  currSeats,
+  // description,
 }) => {
   const isAvailable = reservationStatus === "can reserve";
   const reservationClass = isAvailable ? "bg-green-500" : "bg-red-500";
   const isFullseats = isAvailable ? "text-white" : "text-red-500";
-  const [isHovered, setIsHovered] = useState(false);
+  // const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div
-      className="bg-gray-900 rounded-xl overflow-hidden shadow-lg w-auto m-1 flex-shrink-0 relative"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="relative">
-        <img src={image} alt={title} className="w-full h-52 object-cover" />
-        <div
-          className={`absolute top-3 text-white text-xs font-bold px-2 py-1 rounded-r-lg ${reservationClass}`}
-        >
-          {reservationStatus}
-        </div>
-        <div className="absolute top-2 right-2 bg-gray-900 text-white text-xs font-bold px-2 py-1 rounded">
-          <FontAwesomeIcon icon={faHeart} />
-        </div>
-        <div className="absolute bottom-2 left-2 text-white text-xs font-bold px-2 py-1 rounded">
-          <div className="flex items-center">
-            {[...Array(rating)].map((_, i) => (
-              <FontAwesomeIcon
-                key={i}
-                icon={faStar}
-                className="text-yellow-400"
-              />
-            ))}
+    <Link to={`/shop/${id}`} className="no-underline">
+      <div
+        className="bg-gray-900 rounded-xl overflow-hidden shadow-lg w-auto m-1 flex-shrink-0 relative"
+        // onMouseEnter={() => setIsHovered(true)}
+        // onMouseLeave={() => setIsHovered(false)}
+      >
+        <div className="relative">
+          <img
+            src={image}
+            alt={title}
+            className={
+              "w-full h-52 object-cover hover:scale-105 duration-500 ease-out"
+            }
+          />
+          <div
+            className={`absolute top-3 text-white text-xs font-bold px-2 py-1 rounded-r-lg ${reservationClass}`}
+          >
+            {reservationStatus}
           </div>
-          <p className={`mt-2 ${isFullseats}`}>
-            {currSeats} / {maxSeats}
-          </p>
+          <div className="absolute top-2 right-2 bg-gray-900 text-white text-xs font-bold px-2 py-1 rounded">
+            <FontAwesomeIcon icon={faHeart} />
+          </div>
+          <div className="absolute bottom-2 left-2 text-white text-xs font-bold px-2 py-1 rounded">
+            <div className="flex items-center">
+              {[...Array(rating)].map((_, i) => (
+                <FontAwesomeIcon
+                  key={i}
+                  icon={faStar}
+                  className="text-yellow-400"
+                />
+              ))}
+            </div>
+            <p className={`mt-2 ${isFullseats}`}>
+              {currSeats} / {maxSeats}
+            </p>
+          </div>
         </div>
+        {/* {isHovered && (
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex-col flex items-center justify-center p-4 text-center text-white">
+            <div className="p-4">
+              <h2 className="text-lg font-bold text-white">{title}</h2>
+            </div>
+            <p>{description}</p>
+          </div>
+        )} */}
       </div>
-      {isHovered && (
-        <div className="absolute inset-0 bg-black bg-opacity-75 flex-col flex items-center justify-center p-4 text-center text-white">
-          <div className="p-4">
-            <h2 className="text-lg font-bold text-white">{title}</h2>
-          </div>
-          <p>{description}</p>
-        </div>
-      )}
-    </div>
+    </Link>
   );
 };
 
