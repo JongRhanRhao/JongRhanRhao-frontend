737622dee0db05054ab00c688ea4c35fc9231037
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-08-12 00:58:41 +0700

    Update Sample data

--- src/SampleData/data.ts
+++ src/SampleData/data.ts
@@ -1,21 +1,48 @@
 export const shopData: {
-  [key: number]: { name: string; description: string };
+  [key: number]: {
+    name: string;
+    description: string;
+    image: string;
+    day: string;
+    openTime: string;
+    reserveExpired: string;
+  };
 } = {
   1: {
     name: "Warmup",
     description: "This is a popular club where you can have fun.",
+    image:
+      "https://northspace.life/wp-content/uploads/2024/01/hanguotcnx2024-03-1024x1024.jpg",
+    day: "EVERYDAY",
+    openTime: "19:00 - 02:00",
+    reserveExpired: "20:00",
   },
   2: {
     name: "Thachang",
     description: "This is another popular club where you can have fun.",
+    image:
+      "https://northspace.life/wp-content/uploads/2024/01/hanguotcnx2024-02-1024x1024.jpg",
+    day: "EVERYDAY",
+    openTime: "19:00 - 03:00",
+    reserveExpired: "20:00",
   },
   3: {
     name: "Too Nice Nimman",
     description: "This is a trendy club with a great atmosphere.",
+    image:
+      "https://northspace.life/wp-content/uploads/2024/01/hanguotcnx2024-05-1024x1024.jpg",
+    day: "EVERYDAY",
+    openTime: "19:00 - 02:00",
+    reserveExpired: "20:00",
   },
   4: {
     name: "Lonely Heart Cafe",
     description: "This is a cozy cafe with a romantic ambiance.",
+    image:
+      "https://northspace.life/wp-content/uploads/2024/01/hanguotcnx2024-16-1024x1024.jpg",
+    day: "EVERYDAY",
+    openTime: "19:00 - 02:00",
+    reserveExpired: "20:00",
   },
 };
 
