9eca4cc47491913d408eb12dbb431c1501d32550
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-08-31 19:18:16 +0700

    feat: add comment input

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -14,6 +14,21 @@ const CommentSection = () => {
 
   return (
     <div className="space-y-3">
+      <form className="max-w-2xl rounded-lg">
+        <textarea
+          placeholder="Comment..."
+          className="w-full rounded-lg leading-normal resize-none h-20 py-2 px-3 font-medium placeholder-gray-600 focus:outline-none focus:bg-white bg-accent"
+          defaultValue={""}
+        />
+        <div className="flex justify-end px-4">
+          <input
+            type="submit"
+            className="px-2.5 py-1.5 rounded-md text-white text-sm bg-primary cursor-pointer mt-1"
+            defaultValue="Comment"
+          />
+        </div>
+      </form>
+
       <h2 className="text-secondary text-xl font-medium">Comments</h2>
       {shop.comment && shop.comment.length > 0 ? (
         shop.comment.map((comment, index) => (
