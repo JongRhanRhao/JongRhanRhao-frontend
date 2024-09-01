73fa658f071ba6c54ce841fa975346994d027fd5
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-01 20:57:39 +0700

    chore: add favorite page

--- src/pages/MenuPages/Favorite.tsx
+++ src/pages/MenuPages/Favorite.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import FavoriteStoreList from "@/components/shared/FavoriteStoreList";
+import HeaderCard from "@/components/shared/HeaderCard";
 
 const FavoriteContent = () => {
-  return <div>FavoriteContent</div>;
+  return (
+    <>
+      <div className="space-y-4">
+        <HeaderCard
+          title="Favorite"
+          description="Your favorite store lists."
+          imageSrc="https://png.pngtree.com/png-vector/20220616/ourmid/pngtree-drunk-santa-claus-cartoon-character-with-two-mugs-of-beer-png-image_5090275.png"
+        />
+        <FavoriteStoreList />
+      </div>
+    </>
+  );
 };
 
 export default FavoriteContent;
