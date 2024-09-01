ec4dc14ff7a91685926d36daa5b24bae09cf8d83
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-01 20:56:51 +0700

    chore: change variable

--- src/pages/MenuPages/Dashboard.tsx
+++ src/pages/MenuPages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import Card from "@/components/shared/HeaderCard";
+import HeaderCard from "@/components/shared/HeaderCard";
 import SearchBar from "@/components/shared/SearchBar";
 import PopularStoreList from "@/components/shared/PopularStoreList";
 import SeatAvailableStoreList from "@/components/shared/SeatAvailableStoreList";
@@ -7,7 +7,7 @@ const DashboardContent = () => {
   return (
     <div className="space-y-4">
       <SearchBar />
-      <Card
+      <HeaderCard
         title="Dashboard"
         description="Welcome to dashboard. Here you can see available store and seat status."
         imageSrc="https://png.pngtree.com/png-vector/20220616/ourmid/pngtree-drunk-santa-claus-cartoon-character-with-two-mugs-of-beer-png-image_5090275.png"
