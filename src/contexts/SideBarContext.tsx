f73e39c9b79749469fd879d4b51394777750a249
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-02 17:10:52 +0700

    feat: add side bar context

---
+++ src/contexts/SideBarContext.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+interface SidebarContextProps {
+  leftSidebarExpanded: boolean;
+  toggleLeftSidebar: () => void;
+  rightSidebarExpanded: boolean;
+  toggleRightSidebar: () => void;
+}
+
+const SidebarContext = createContext<SidebarContextProps | undefined>(
+  undefined
+);
+
+export const useSidebarContext = () => {
+  const context = useContext(SidebarContext);
+  if (!context) {
+    throw new Error("useSidebarContext must be used within a SidebarProvider");
+  }
+  return context;
+};
+
+export const SidebarProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [leftSidebarExpanded, setLeftSidebarExpanded] = useState(false);
+  const [rightSidebarExpanded, setRightSidebarExpanded] = useState(true);
+
+  const toggleLeftSidebar = () => setLeftSidebarExpanded((prev) => !prev);
+  const toggleRightSidebar = () => setRightSidebarExpanded((prev) => !prev);
+  return (
+    <SidebarContext.Provider
+      value={{
+        leftSidebarExpanded,
+        toggleLeftSidebar,
+        rightSidebarExpanded,
+        toggleRightSidebar,
+      }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  );
+};
