0d0c7134905a1d4e4a4424d1ba9c2e40f1f1935f
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-02 17:11:08 +0700

    feat: implement left side bar to can show/hide

--- src/components/layout/LeftSidebar.tsx
+++ src/components/layout/LeftSidebar.tsx
@@ -1,14 +1,18 @@
-import { Link, useLocation } from "react-router-dom";
-import classNames from "classnames";
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faTachometerAlt,
   faStar,
   faEnvelope,
   faFileInvoiceDollar,
   faCog,
   faStore,
+  faBars,
+  faX,
+  faHome,
 } from "@fortawesome/free-solid-svg-icons";
+
+import { useSidebarContext } from "@/contexts/SideBarContext";
 import UpgradeToVIPCard from "@/components/shared/UpgradeToVIPCard";
 
 interface LeftSidebarProps {
@@ -17,12 +21,13 @@ interface LeftSidebarProps {
 
 const LeftSidebar: React.FC<LeftSidebarProps> = ({ onItemClick }) => {
   const location = useLocation();
+  const { leftSidebarExpanded, toggleLeftSidebar } = useSidebarContext();
 
   const items = [
     {
       name: "Dashboard",
       key: "Item 1",
-      icon: faTachometerAlt,
+      icon: faHome,
       path: "/dashboard",
     },
     { name: "Favorite", key: "Item 2", icon: faStar, path: "/favorite" },
@@ -38,40 +43,81 @@ const LeftSidebar: React.FC<LeftSidebarProps> = ({ onItemClick }) => {
   ];
 
   return (
-    <div className="w-64 bg-accent text-white p-4 text-center flex flex-col">
-      <Link
-        to="/dashboard"
-        className="text-3xl text-primary font-bold font-sans mt-5 text-left"
+    <div
+      className={`flex flex-col transition-all duration-300 ${
+        leftSidebarExpanded ? "w-64" : "w-20"
+      } bg-accent text-white p-4`}
+    >
+      <button
+        onClick={toggleLeftSidebar}
+        className={`text-xl text-secondary flex ${
+          leftSidebarExpanded ? "justify-end" : "justify-center"
+        }`}
       >
-        JongRhanRhao
-      </Link>
-      <ul className="mt-4 text-md font-sans text-left space-y-4">
-        {items.map((item) => {
-          const isSelectedMenuAndPathMatch = location.pathname === item.path;
-
-          return (
-            <li
-              key={item.key}
-              className={classNames(
-                "cursor-pointer p-4 rounded-xl flex items-center space-x-2",
-                {
-                  "bg-primary": isSelectedMenuAndPathMatch,
-                }
-              )}
-              onClick={() => onItemClick(item.key)}
-            >
-              <Link
-                to={item.path}
-                className="flex items-center space-x-2 w-full"
+        <FontAwesomeIcon icon={leftSidebarExpanded ? faX : faBars} />
+      </button>
+      <div className="flex justify-between items-baseline">
+        <Link
+          to="/dashboard"
+          className={`text-2xl font-bold font-sans mt-5 text-left ${
+            leftSidebarExpanded ? "text-primary" : "hidden"
+          }`}
+        >
+          JongRhanRhao
+        </Link>
+      </div>
+      <div
+        className={`mt-4  ${
+          leftSidebarExpanded ? "" : "hidden"
+        } text-md font-sans text-left space-y-4`}
+      >
+        <ul className="space-y-3">
+          {items.map((item) => {
+            const isSelectedMenuAndPathMatch = location.pathname === item.path;
+            return (
+              <li
+                key={item.key}
+                className={`cursor-pointer p-4 rounded-xl flex items-center space-x-2 ${
+                  isSelectedMenuAndPathMatch ? "bg-primary" : ""
+                }`}
+                onClick={() => onItemClick(item.key)}
               >
-                <FontAwesomeIcon icon={item.icon} />
-                <span>{item.name}</span>
-              </Link>
-            </li>
-          );
-        })}
-        <UpgradeToVIPCard />
-      </ul>
+                <Link
+                  to={item.path}
+                  className="flex items-center space-x-2 w-full"
+                >
+                  <FontAwesomeIcon icon={item.icon} />
+                  {leftSidebarExpanded && <span>{item.name}</span>}
+                </Link>
+              </li>
+            );
+          })}
+          <UpgradeToVIPCard />
+        </ul>
+      </div>
+      {!leftSidebarExpanded && (
+        <ul className="space-y-4 mt-8">
+          {items.map((item) => {
+            const isSelectedMenuAndPathMatch = location.pathname === item.path;
+            return (
+              <li
+                key={item.key}
+                className={`cursor-pointer p-4 rounded-xl flex items-center space-x-2 ${
+                  isSelectedMenuAndPathMatch ? "bg-primary" : ""
+                }`}
+                onClick={() => onItemClick(item.key)}
+              >
+                <Link
+                  to={item.path}
+                  className="flex items-center space-x-2 w-full"
+                >
+                  <FontAwesomeIcon icon={item.icon} />
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
