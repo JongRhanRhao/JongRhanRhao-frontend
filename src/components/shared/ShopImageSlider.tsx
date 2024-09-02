4817846fc669d5b52da575fd9cfcfc874d091de2
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-03 00:54:04 +0700

    feat: add shop image slide

---
+++ src/components/shared/ShopImageSlider.tsx
@@ -0,0 +1,58 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import ShopCard from "@/components/shared/ShopCard";
+import { ShopCards } from "@/SampleData/data";
+
+const ShopImageSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 500,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="p-4">
+      <div className="flex justify-between"></div>
+      <Slider {...settings}>
+        {ShopCards.map((card) => (
+          <div key={card.id} className="px-1">
+            <ShopCard
+              id={card.id}
+              image={card.image}
+              title={card.title}
+              reservationStatus={card.reservationStatus}
+              rating={card.rating}
+              currSeats={card.currSeats}
+              maxSeats={card.maxSeats}
+              description={card.description}
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default ShopImageSlider;
