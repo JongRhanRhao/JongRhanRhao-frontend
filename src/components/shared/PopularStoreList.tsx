2c58bc973a3a4d984a77a8aea68e952ae21fb6e4
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-03 00:53:34 +0700

    refactor: show popular store as gallery

--- src/components/shared/PopularStoreList.tsx
+++ src/components/shared/PopularStoreList.tsx
@@ -1,37 +1,7 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import StoreCard from "@/components/shared/ShopCard";
+import ShopCard from "@/components/shared/ShopCard";
 import { ShopCards } from "@/SampleData/data";
 
 const PopularStoreList = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 500,
-    cssEase: "linear",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <div className="p-4">
       <div className="flex justify-between">
@@ -42,22 +12,21 @@ const PopularStoreList = () => {
           </span>
         </div>
       </div>
-      <Slider {...settings}>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {ShopCards.map((card) => (
-          <div key={card.id} className="px-1">
-            <StoreCard
-              id={card.id}
-              image={card.image}
-              title={card.title}
-              reservationStatus={card.reservationStatus}
-              rating={card.rating}
-              currSeats={card.currSeats}
-              maxSeats={card.maxSeats}
-              description={card.description}
-            />
-          </div>
+          <ShopCard
+            key={card.id}
+            id={card.id}
+            image={card.image}
+            title={card.title}
+            reservationStatus={card.reservationStatus}
+            rating={card.rating}
+            currSeats={card.currSeats}
+            maxSeats={card.maxSeats}
+            description={card.description}
+          />
         ))}
-      </Slider>
+      </div>
     </div>
   );
 };
