10fe2397028de7a16b5564114bf8cae8039f7bfb
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-03 01:23:33 +0700

    style: change gap

--- src/components/shared/PopularStoreList.tsx
+++ src/components/shared/PopularStoreList.tsx
@@ -12,7 +12,7 @@ const PopularStoreList = () => {
           </span>
         </div>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
         {ShopCards.map((card) => (
           <ShopCard
             key={card.id}
