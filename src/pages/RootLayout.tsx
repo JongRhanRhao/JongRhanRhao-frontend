7dfd880ca5948a906e369c82d64ede1518cf6604
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-02 17:11:30 +0700

    feat: add sidebar provider

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,27 +1,30 @@
-import { FC } from "react";
+import React from "react";
 
 import LeftSidebar from "@/components/layout/LeftSidebar";
 import RightSidebar from "@/components/layout/RightSidebar";
 import Content from "@/components/layout/Content";
+import { SidebarProvider } from "@/contexts/SideBarContext";
 
 interface RootLayoutProps {
   selectedItem: string;
   onItemClick: (item: string) => void;
 }
 
-const RootLayout: FC<RootLayoutProps> = ({ onItemClick }) => {
+const RootLayout: React.FC<RootLayoutProps> = ({ onItemClick }) => {
   return (
-    <div className="flex justify-center h-screen overflow-hidden">
-      <div className="flex w-full h-full">
-        <LeftSidebar onItemClick={onItemClick} />
-        <div className="flex-1 flex flex-col overflow-hidden">
-          <main className="flex-1 p-4 bg-accent2 overflow-y-auto">
-            <Content />
-          </main>
+    <SidebarProvider>
+      <div className="flex justify-center h-screen overflow-hidden">
+        <div className="flex w-full h-full">
+          <LeftSidebar onItemClick={onItemClick} />
+          <div className="flex-1 flex flex-col overflow-hidden">
+            <main className="flex-1 p-4 bg-accent2 overflow-y-auto">
+              <Content />
+            </main>
+          </div>
+          <RightSidebar />
         </div>
-        <RightSidebar />
       </div>
-    </div>
+    </SidebarProvider>
   );
 };
 
