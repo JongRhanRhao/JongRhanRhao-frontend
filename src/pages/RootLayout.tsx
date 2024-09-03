5024706dd2ec8a4dd1a87e4f8e5599aeaec035db
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-04 02:49:34 +0700

    feat: add footer

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
 import LeftSidebar from "@/components/layout/LeftSidebar";
 import RightSidebar from "@/components/layout/RightSidebar";
 import Content from "@/components/layout/Content";
+import Footer from "@/components/Footer";
 import { SidebarProvider } from "@/contexts/SideBarContext";
 
 interface RootLayoutProps {
@@ -17,8 +17,11 @@ const RootLayout: React.FC<RootLayoutProps> = ({ onItemClick }) => {
         <div className="flex w-full h-full">
           <LeftSidebar onItemClick={onItemClick} />
           <div className="flex-1 flex flex-col overflow-hidden">
-            <main className="flex-1 p-4 bg-accent2 overflow-y-auto">
-              <Content />
+            <main className="flex-1 flex flex-col p-4 bg-accent2 overflow-y-auto">
+              <div className="flex-grow">
+                <Content />
+              </div>
+              <Footer />
             </main>
           </div>
           <RightSidebar />
