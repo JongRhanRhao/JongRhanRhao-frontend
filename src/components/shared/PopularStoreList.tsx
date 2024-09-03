928566b61749051e92746e98989eaf377e83cfaa
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-04 00:49:36 +0700

    chore: remove unuse code

--- src/components/shared/PopularStoreList.tsx
+++ src/components/shared/PopularStoreList.tsx
@@ -4,14 +4,7 @@ import { ShopCards } from "@/SampleData/data";
 const PopularStoreList = () => {
   return (
     <div className="p-4">
-      <div className="flex justify-between">
-        <h2 className="text-4xl font-bold text-primary mb-2">Popular</h2>
-        <div className="flex justify-between items-center mb-4">
-          <span className="text-primary hover:underline cursor-pointer">
-            View all
-          </span>
-        </div>
-      </div>
+      <h2 className="text-4xl font-bold text-primary mb-5">Popular</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
         {ShopCards.map((card) => (
           <ShopCard
