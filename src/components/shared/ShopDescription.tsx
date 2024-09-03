ac427b5f7bd6192a0dbb53d9d5d1d87518dbe73b
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-04 00:32:38 +0700

    style: better styling

--- src/components/shared/ShopDescription.tsx
+++ src/components/shared/ShopDescription.tsx
@@ -1,10 +1,19 @@
 import { FC } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 import LeftSidebar from "@/components/layout/LeftSidebar";
 import RightSidebar from "@/components/layout/RightSidebar";
 import { shopData } from "@/SampleData/data";
 import CommentSection from "@/components/CommentSection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleLeft,
+  faCalendarTimes,
+  faClock,
+  faInfoCircle,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+import BookingButton from "../BookingButton";
 
 interface ShopDescriptionProps {
   selectedItem: string;
@@ -21,36 +30,58 @@ const ShopDescription: FC<ShopDescriptionProps> = ({ onItemClick }) => {
   const isBusy = isAvailable ? "text-red-500" : "text-green-500";
 
   return (
-    <div className="flex justify-center h-screen overflow-hidden">
+    <div className="flex justify-center h-screen">
       <div className="flex w-full h-full">
         <LeftSidebar onItemClick={onItemClick} />
         <div className="flex-1 flex flex-col overflow-hidden">
-          <main className="flex-1 p-4 bg-accent2 overflow-y-auto space-y-10">
+          <main className="flex-1 p-4 bg-accent2">
+            <Link to="/">
+              <div className="py-2 text-xl text-primary mb-2">
+                <FontAwesomeIcon icon={faAngleLeft} />
+                &nbsp; Back
+              </div>
+            </Link>
             <div className="flex justify-center">
-              <img src={shop.image} className="rounded-xl h-96"></img>
-              <div className="bg-secondary rounded-xl p-3 h-36 w-full ml-5">
-                <div className="font-bold text-accent uppercase text-lg">
+              <img
+                src={shop.image}
+                className="rounded-xl h-96"
+                alt="shopImage"
+              ></img>
+              <div className="rounded-xl p-3 h-36 w-full ml-5">
+                <div className="font-bold text-gray-300 uppercase text-4xl">
                   {shop.name}
                 </div>
-                <div className="font-bold text-gray-500">
-                  Opening Hours :{" "}
-                  <span className="text-primary font-bold">
+                <div className="mb-4 mt-2">{shop.description}</div>
+                <div className="font-bold text-gray-300">
+                  <FontAwesomeIcon icon={faClock} />
+                  &nbsp; Opening Hours :{" "}
+                  <span className="text-gray-400 font-bold">
                     {shop.openTime}
                   </span>
                 </div>
-                <div className="font-bold text-gray-500">
-                  Reservation Expired time:{" "}
-                  <span className="text-primary font-bold">
+                <div className="font-bold text-gray-300">
+                  <FontAwesomeIcon icon={faCalendarTimes} />
+                  &nbsp; Reservation Expired time:{" "}
+                  <span className="text-gray-400 font-bold">
                     {shop.reserveExpired}
                   </span>
                 </div>
-                <div className="font-bold text-gray-500">
-                  Status:{" "}
+                <div className="font-bold text-gray-300">
+                  <FontAwesomeIcon icon={faInfoCircle} />
+                  &nbsp; Status:{" "}
                   <span className={`${isBusy} font-bold`}>{shop.status}</span>
                 </div>
+                <div className="font-bold text-gray-300">
+                  <FontAwesomeIcon icon={faLocationDot} />
+                  &nbsp; Address:{" "}
+                  <span className={`${isBusy} font-bold`}>{shop.status}</span>
+                </div>
+                <div className="mt-7">
+                  <BookingButton />
+                </div>
               </div>
+              {/* Comment Section */}
             </div>
-            {/* Comment Section */}
             <CommentSection />
           </main>
         </div>
