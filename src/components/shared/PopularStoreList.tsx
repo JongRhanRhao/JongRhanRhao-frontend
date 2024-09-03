3022901e9d9e4ca1207d3b48d4c4986c9e87cf74
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-04 01:03:44 +0700

    feat: add fake loader

--- src/components/shared/PopularStoreList.tsx
+++ src/components/shared/PopularStoreList.tsx
@@ -1,25 +1,43 @@
+import { useState, useEffect } from "react";
+
 import ShopCard from "@/components/shared/ShopCard";
 import { ShopCards } from "@/SampleData/data";
 
 const PopularStoreList = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="p-4">
       <h2 className="text-4xl font-bold text-primary mb-5">Popular</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-        {ShopCards.map((card) => (
-          <ShopCard
-            key={card.id}
-            id={card.id}
-            image={card.image}
-            title={card.title}
-            reservationStatus={card.reservationStatus}
-            rating={card.rating}
-            currSeats={card.currSeats}
-            maxSeats={card.maxSeats}
-            description={card.description}
-          />
-        ))}
-      </div>
+      {isLoading ? (
+        <div className="flex justify-center items-center">
+          <span className="loading loading-ring loading-lg text-secondary"></span>
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+          {ShopCards.map((card) => (
+            <ShopCard
+              key={card.id}
+              id={card.id}
+              image={card.image}
+              title={card.title}
+              reservationStatus={card.reservationStatus}
+              rating={card.rating}
+              currSeats={card.currSeats}
+              maxSeats={card.maxSeats}
+              description={card.description}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
