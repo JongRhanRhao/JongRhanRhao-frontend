ef45e4ee98ec8c184f0b308d02239e72cea57e53
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-04 02:47:54 +0700

    refactor: optimize code to use memo

--- src/components/shared/PopularStoreList.tsx
+++ src/components/shared/PopularStoreList.tsx
@@ -1,7 +1,10 @@
-import { useState, useEffect } from "react";
-
+import { useState, useEffect, useMemo, memo } from "react";
 import ShopCard from "@/components/shared/ShopCard";
 import { ShopCards } from "@/SampleData/data";
+
+const LOADING_TIMEOUT = 500;
+
+const MemoizedShopCard = memo(ShopCard);
 
 const PopularStoreList = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -9,37 +12,59 @@ const PopularStoreList = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 500);
+    }, LOADING_TIMEOUT);
 
     return () => clearTimeout(timer);
   }, []);
 
-  return (
-    <div className="p-4">
-      <h2 className="text-4xl font-bold text-primary mb-5">Popular</h2>
-      {isLoading ? (
+  const shopCardsList = useMemo(
+    () =>
+      ShopCards.map(
+        ({
+          id,
+          image,
+          title,
+          reservationStatus,
+          rating,
+          currSeats,
+          maxSeats,
+          description,
+        }) => (
+          <MemoizedShopCard
+            key={id}
+            id={id}
+            image={image}
+            title={title}
+            reservationStatus={reservationStatus}
+            rating={rating}
+            currSeats={currSeats}
+            maxSeats={maxSeats}
+            description={description}
+          />
+        )
+      ),
+    []
+  );
+
+  if (isLoading) {
+    return (
+      <div className="p-4">
+        <h2 className="text-4xl font-bold text-primary mb-5">Popular</h2>
         <div className="flex justify-center items-center">
           <span className="loading loading-ring loading-lg text-secondary"></span>
         </div>
-      ) : (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-          {ShopCards.map((card) => (
-            <ShopCard
-              key={card.id}
-              id={card.id}
-              image={card.image}
-              title={card.title}
-              reservationStatus={card.reservationStatus}
-              rating={card.rating}
-              currSeats={card.currSeats}
-              maxSeats={card.maxSeats}
-              description={card.description}
-            />
-          ))}
-        </div>
-      )}
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4">
+      <h2 className="text-4xl font-bold text-primary mb-5">Popular</h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+        {shopCardsList}
+      </div>
     </div>
   );
 };
 
-export default PopularStoreList;
+export default memo(PopularStoreList);
