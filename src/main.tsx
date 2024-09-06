113b8d35e94eac3a5e398f4c30dc53665d1e2f67
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-07 03:57:50 +0700

    refactor: Update import paths and add global URL routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { SidebarProvider } from "./contexts/SideBarContext.tsx";
+
+import { SidebarProvider } from "@/contexts/SideBarContext.tsx";
+import { UserProvider } from "@/contexts/UserContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SidebarProvider>
-      <App />
-    </SidebarProvider>
+    <UserProvider>
+      <QueryClientProvider client={queryClient}>
+        <SidebarProvider>
+          <App />
+        </SidebarProvider>
+      </QueryClientProvider>
+    </UserProvider>
   </React.StrictMode>
 );
