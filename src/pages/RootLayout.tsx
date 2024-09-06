c95cb65351616d03a691152f707e0089d8d321ef
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-07 03:59:32 +0700

    refactor: Update RootLayout component layout and add responsive behavior

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import LeftSidebar from "@/components/layout/LeftSidebar";
 import RightSidebar from "@/components/layout/RightSidebar";
 import Content from "@/components/layout/Content";
-import Footer from "@/components/Footer";
+import Footer from "@/components/shared/Footer";
 import { SidebarProvider } from "@/contexts/SideBarContext";
+import BottomMenu from "@/components/shared/BottomMenu";
 
 interface RootLayoutProps {
   selectedItem: string;
@@ -15,7 +15,9 @@ const RootLayout: React.FC<RootLayoutProps> = ({ onItemClick }) => {
     <SidebarProvider>
       <div className="flex justify-center h-screen overflow-hidden">
         <div className="flex w-full h-full">
-          <LeftSidebar onItemClick={onItemClick} />
+          <div className="hidden lg:flex">
+            <LeftSidebar onItemClick={onItemClick} />
+          </div>
           <div className="flex-1 flex flex-col overflow-hidden">
             <main className="flex-1 flex flex-col p-4 bg-accent2 overflow-y-auto">
               <div className="flex-grow">
@@ -24,7 +26,12 @@ const RootLayout: React.FC<RootLayoutProps> = ({ onItemClick }) => {
               <Footer />
             </main>
           </div>
-          <RightSidebar />
+          <div className="hidden lg:flex">
+            <RightSidebar />
+          </div>
+          <div className="lg:hidden fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full">
+            <BottomMenu onItemClick={onItemClick} />
+          </div>
         </div>
       </div>
     </SidebarProvider>
