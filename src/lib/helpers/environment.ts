5101ebb01ccf9d8f752305b83de0536ef3fa98f8
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-07 03:59:09 +0700

    refactor: Update import paths and add global URL routes

--- src/lib/helpers/environment.ts
+++ src/lib/helpers/environment.ts
@@ -1,1 +1,10 @@
 export const SERVER_URL: string = "http://localhost:3000";
+
+export const GLOBAL_URL_ROUTES = {
+  landingPage: "/",
+  message: "/message",
+  reserveStatus: "/ReserveStatus",
+  storeManagement: "/StoreManagement",
+  setting: "/setting",
+  notFound: "*",
+};
