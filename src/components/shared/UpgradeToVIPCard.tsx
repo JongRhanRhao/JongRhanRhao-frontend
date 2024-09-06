ae69e1df5a27f7832e840345d584b2af925fd4fb
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-07 03:58:14 +0700

    refactor: Update UpgradeToVIPCard component layout

--- src/components/shared/UpgradeToVIPCard.tsx
+++ src/components/shared/UpgradeToVIPCard.tsx
@@ -1,13 +1,15 @@
 const UpgradeToVIPCard = () => {
   return (
-    <div className="p-4 bg-primary shadow-lg rounded-xl w-10/12 h-32 overflow-hidden max-w-8xl bg-pattern">
-      <header className="text-secondary font-bold text-md text-left font-sans">
-        Upgrade your account to VIP
-      </header>
-      <div className="flex justify-end mt-4">
-        <button className="bg-secondary text-black text-sm font-bold px-2 py-1 rounded-md font-sans hover:text-accent2">
-          Upgrade
-        </button>
+    <div className="flex justify-center">
+      <div className="p-4 bg-primary shadow-lg rounded-xl w-10/12 h-32 overflow-hidden max-w-8xl bg-pattern">
+        <header className="text-secondary font-bold text-md text-left font-sans">
+          Upgrade your account to VIP
+        </header>
+        <div className="flex justify-end mt-4">
+          <button className="bg-secondary text-black text-sm font-bold px-2 py-1 rounded-md font-sans hover:text-accent2">
+            Upgrade
+          </button>
+        </div>
       </div>
     </div>
   );
