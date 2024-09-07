9dff9f9a598b70cf8026882f6599c5c5c263403c
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-07 20:25:12 +0700

    refactor: Update ShopImageSlider component to fetch store data from server and handle loading and error states

--- src/components/shared/ShopImageSlider.tsx
+++ src/components/shared/ShopImageSlider.tsx
@@ -3,9 +3,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import ShopCard from "@/components/shared/ShopCard";
-import { ShopSamepleData } from "@/SampleData/data";
+import { useFetchStores } from "@/hooks/useFetchStores";
 
 const ImageSlider: React.FC = () => {
+  const { data: stores, isLoading, error } = useFetchStores();
   const settings = {
     infinite: true,
     slidesToShow: 1,
@@ -16,12 +17,43 @@ const ImageSlider: React.FC = () => {
     cssEase: "linear",
   };
 
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <span className="loading loading-ring loading-lg text-primary"></span>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="text-2xl flex justify-center items-center h-full flex-col">
+        Something went wrong, please try again later.
+      </div>
+    );
+  }
+
   return (
-    <div className={`p-2`}>
+    <div className="p-2">
       <Slider {...settings}>
-        {ShopSamepleData.map((card) => (
-          <div key={card.id}>
-            <ShopCard {...card} />
+        {stores?.map((store) => (
+          <div key={store.store_id}>
+            <ShopCard
+              id={store.store_id}
+              image={store.image_url || ""}
+              title={store.shop_name}
+              reservationStatus={
+                store.curr_seats < store.max_seats
+                  ? "can reserve"
+                  : "cannot reserve"
+              }
+              rating={store.rating}
+              maxSeats={store.max_seats}
+              currSeats={store.curr_seats}
+              description={store.description || ""}
+              isFavorite={store.is_favorite}
+              onClick={() => {}}
+            />
           </div>
         ))}
       </Slider>
