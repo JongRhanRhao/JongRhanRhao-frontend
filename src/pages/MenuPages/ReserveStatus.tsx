43239f65b30bb7d13ef8f5896233d9d484597e44
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-09 03:13:48 +0700

    refactor: Add authentication check and "My Store" link in ReservationStatus component

--- src/pages/MenuPages/ReserveStatus.tsx
+++ src/pages/MenuPages/ReserveStatus.tsx
@@ -1,12 +1,14 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
 import BackHomeButton from "@/components/shared/BackHomeButton";
 import { useUser } from "@/contexts/UserContext";
 import { SERVER_URL } from "@/lib/helpers/environment";
 import { Reservations } from "@/lib/types";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 
 const ReservationStatus = () => {
   const user = useUser();
+  const { isAuthenticated } = user;
   const userId = user.user?.userId;
   const { data, isLoading, error } = useQuery({
     queryKey: ["reservations"],
@@ -15,6 +17,15 @@ const ReservationStatus = () => {
         .get(`${SERVER_URL}/stores/api/reservations/customer/${userId}`)
         .then((res) => res.data),
   });
+
+  if (!isAuthenticated) {
+    return (
+      <div className="text-2xl text-text flex justify-center items-center h-full flex-col">
+        You need to log in to view this page.
+        <BackHomeButton className="mt-5 text-primary" />
+      </div>
+    );
+  }
 
   if (isLoading) {
     return (
@@ -66,10 +77,12 @@ const ReservationStatus = () => {
       </div>
     );
   }
-
+  // TODO: Add refresh button, control button
   return (
-    <div className="p-4 space-x-2 space-y-4">
-      <div className="text-text text-3xl font-bold">Manage Reservations</div>
+    <div className="p-4">
+      <div className="text-text text-3xl font-bold mb-4">
+        Manage Reservations
+      </div>
       <table className="w-full table-fixed">
         <thead>
           <tr className="bg-secondary">
@@ -85,9 +98,10 @@ const ReservationStatus = () => {
             <th className="w-1/4 py-4 px-6 text-left text-text font-bold uppercase">
               Status
             </th>
+            <th className="w-1/4 text-left text-text font-bold uppercase"></th>
           </tr>
         </thead>
-        <tbody className="bg-secondary">
+        <tbody className="bg-secondary text-text">
           {data.map((data: Reservations) => (
             <tr key={data.reservation_id}>
               <td className="py-4 px-6 border-b border-neutral-500">
@@ -97,12 +111,20 @@ const ReservationStatus = () => {
                 {data.shop_name}
               </td>
               <td className="py-4 px-6 border-b border-neutral-500">
-                {data.reservation_date} {data.reservation_time}
+                {data.reservation_date}, {data.reservation_time}
               </td>
               <td className="py-4 px-6 border-b border-neutral-500">
                 <span className="bg-green-500 text-white py-1 px-2 rounded-full text-sm">
                   {data.reservation_status}
                 </span>
+              </td>
+              <td className="border-b border-neutral-500">
+                <a
+                  href={`/shop/${data.shop_id}`}
+                  className="text-primary underline"
+                >
+                  View Shop
+                </a>
               </td>
             </tr>
           ))}
