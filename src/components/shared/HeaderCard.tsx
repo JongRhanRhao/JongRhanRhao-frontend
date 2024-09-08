90cf3fd4cd01eabec477bc93c38cc512c5c73e88
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-08 22:25:34 +0700

    refactor: Update HeaderCard component styling and text color

--- src/components/shared/HeaderCard.tsx
+++ src/components/shared/HeaderCard.tsx
@@ -10,10 +10,10 @@ const HeaderCard = ({
   return (
     <div className="p-4 bg-primary shadow-lg rounded-xl overflow-hidden max-w-8xl mx-auto items-center py-4 grid-cols-2 flex justify-between bg-pattern">
       <div className="flex flex-col justify-between">
-        <header className="p-4 text-secondary font-bold text-3xl font-sans">
+        <header className="p-4 text-text font-bold text-3xl font-sans">
           {title}
         </header>
-        <p className="p-4 text-secondary font-sans text-sm">{description}</p>
+        <p className="p-4 text-text font-sans text-sm">{description}</p>
       </div>
       <div>
         <img className="rounded-xl h-44" src={imageSrc} alt="logo" />
