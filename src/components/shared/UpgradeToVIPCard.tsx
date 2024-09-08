f1adc24df778707288229c09a5c52f89ef900b75
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-08 22:25:38 +0700

    refactor: Update UpgradeToVIPCard component styling and button text color

--- src/components/shared/UpgradeToVIPCard.tsx
+++ src/components/shared/UpgradeToVIPCard.tsx
@@ -2,11 +2,11 @@ const UpgradeToVIPCard = () => {
   return (
     <div className="flex justify-center">
       <div className="p-4 bg-primary shadow-lg rounded-xl w-10/12 h-32 overflow-hidden max-w-8xl bg-pattern">
-        <header className="text-secondary font-bold text-md text-left font-sans">
+        <header className="text-text font-bold text-md text-left font-sans">
           Upgrade your account to VIP
         </header>
         <div className="flex justify-end mt-4">
-          <button className="bg-secondary text-black text-sm font-bold px-2 py-1 rounded-md font-sans hover:text-accent2">
+          <button className="bg-text text-bg text-sm font-bold px-2 py-1 rounded-md font-sans hover:text-slate-800">
             Upgrade
           </button>
         </div>
