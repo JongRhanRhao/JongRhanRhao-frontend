4fdf71a4a62c3706a1099d24008073bce28c10cc
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-10 15:19:54 +0700

    refactor: Remove UserProvider and SidebarProvider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { SidebarProvider } from "@/contexts/SideBarContext.tsx";
-import { UserProvider } from "@/contexts/UserContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserProvider>
-      <QueryClientProvider client={queryClient}>
-        <SidebarProvider>
-          <App />
-        </SidebarProvider>
-      </QueryClientProvider>
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
