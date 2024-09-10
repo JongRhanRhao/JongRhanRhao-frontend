4cde4cdc03e323ef2e5e24e58926dddf7d16dd76
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-10 15:22:09 +0700

    refactor: Remove SidebarProvider from RootLayout

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -4,7 +4,6 @@ import LeftSidebar from "@/components/layout/LeftSidebar";
 import RightSidebar from "@/components/layout/RightSidebar";
 import Content from "@/components/layout/Content";
 import Footer from "@/components/shared/Footer";
-import { SidebarProvider } from "@/contexts/SideBarContext";
 import SmallScreenMenu from "@/components/shared/SmallScreenMenu";
 
 interface RootLayoutProps {
@@ -14,27 +13,25 @@ interface RootLayoutProps {
 // TODO: add chat with admin feature
 const RootLayout: React.FC<RootLayoutProps> = ({ onItemClick }) => {
   return (
-    <SidebarProvider>
-      <div className="flex h-screen overflow-hidden bg-bg">
-        <LeftSidebar onItemClick={onItemClick} className="hidden lg:block" />
+    <div className="flex h-screen overflow-hidden bg-bg">
+      <LeftSidebar onItemClick={onItemClick} className="hidden lg:block" />
 
-        <main className="flex-1 flex flex-col overflow-hidden">
-          <div className="flex-1 overflow-y-auto">
-            <div className="px-4 py-6 md:px-6 lg:px-8">
-              <Content />
-            </div>
-            <Footer className="mt-8" />
+      <main className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 overflow-y-auto">
+          <div className="px-4 py-6 md:px-6 lg:px-8">
+            <Content />
           </div>
-        </main>
+          <Footer className="mt-8" />
+        </div>
+      </main>
 
-        <RightSidebar className="hidden lg:block md:block" />
+      <RightSidebar className="hidden lg:block md:block" />
 
-        <SmallScreenMenu
-          onItemClick={onItemClick}
-          className="block md:hidden lg:hidden"
-        />
-      </div>
-    </SidebarProvider>
+      <SmallScreenMenu
+        onItemClick={onItemClick}
+        className="block md:hidden lg:hidden"
+      />
+    </div>
   );
 };
 
