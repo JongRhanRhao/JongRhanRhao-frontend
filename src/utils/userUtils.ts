3de64070c25817d36f7f4abff8c651fc13f1caac
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-10 15:27:23 +0700

    refactor: Remove unused userUtils file

--- src/utils/userUtils.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "@/hooks/useUserStore";
-
-export const storeUserData = (user: User) => {
-  localStorage.setItem("user", JSON.stringify(user));
-};
-
-export const clearUserData = () => {
-  localStorage.removeItem("user");
-};
