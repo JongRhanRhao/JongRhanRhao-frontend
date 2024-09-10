48a492827c2bdd39d6fec3afe91cb9b211329fe5
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-10 14:25:03 +0700

    feat: Add useFetchFavoriteStore hook

---
+++ src/hooks/useFetchFavoriteStore.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { SERVER_URL } from "@/lib/variables";
+
+interface FavoriteStore {
+  customer_id: string;
+  store_id: string;
+}
+
+export const useFetchFavoriteStore = (customerId: string) => {
+  return useQuery<FavoriteStore[]>({
+    queryFn: () =>
+      fetch(`${SERVER_URL}/stores/api/favorites/customer/${customerId}`).then(
+        (res) => res.json()
+      ),
+    queryKey: ["favorites", customerId],
+  });
+};
