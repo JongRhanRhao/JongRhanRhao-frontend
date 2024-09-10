ba139d9d3c700563122a21e01b7a85d55b39080d
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-10 16:31:50 +0700

    refactor: Update Facebook login button UI and fix viewBox issue

--- src/components/shared/LoginButton.tsx
+++ src/components/shared/LoginButton.tsx
@@ -159,23 +159,29 @@ const LoginButton = () => {
               className="btn bg-secondary flex items-center justify-center border rounded-md w-full py-3 text-text shadow-sm hover:bg-opacity-50 focus:outline-none focus:ring-2 focus:ring-gray-300"
             >
               <svg
-                className="h-6 w-6 mr-2"
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 48 48"
-                version="1.1"
+                viewBox="-0.5 0 48 48"
+                className="h-6 w-6 mr-2"
               >
-                <g id="Icons" stroke="none" fill="none">
-                  <g
-                    id="Color-"
-                    transform="translate(-200.000000, -160.000000)"
-                    fill="#4460A0"
-                  >
-                    <path
-                      d="M225.638355,208 L202.649232,208 C201.185673,208 200,206.813592 200,205.350603 L200,162.649211 C200,161.18585 201.185859,160 202.649232,160 L245.350955,160 C246.813955,160 248,161.18585 248,162.649211 L248,205.350603 C248,206.813778 246.813769,208 245.350955,208 L233.119305,208 L233.119305,189.411755 L239.358521,189.411755 L240.292755,182.167586 L233.119305,182.167586 L233.119305,177.542641 C233.119305,175.445287 233.701712,174.01601 236.70929,174.01601 L240.545311,174.014333 L240.545311,167.535091 C239.881886,167.446808 237.604784,167.24957 234.955552,167.24957 C229.424834,167.24957 225.638355,170.625526 225.638355,176.825209 L225.638355,182.167586 L219.383122,182.167586 L219.383122,189.411755 L225.638355,189.411755 L225.638355,208 L225.638355,208 Z"
-                      id="Facebook"
-                    ></path>
-                  </g>
-                </g>
+                <linearGradient
+                  id="Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1"
+                  x1="9.993"
+                  x2="40.615"
+                  y1="9.993"
+                  y2="40.615"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop offset="0" stop-color="#2aa4f4"></stop>
+                  <stop offset="1" stop-color="#007ad9"></stop>
+                </linearGradient>
+                <path
+                  fill="url(#Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1)"
+                  d="M24,4C12.954,4,4,12.954,4,24s8.954,20,20,20s20-8.954,20-20S35.046,4,24,4z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46 c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.452 C21.988,43.9,22.981,44,24,44c0.921,0,1.82-0.084,2.707-0.204V29.301z"
+                ></path>
               </svg>
               Continue with Facebook
             </button>
