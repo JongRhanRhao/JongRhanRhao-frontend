30b61d966d30b9f99280278b32c5d2d98b813fd7
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 20:47:27 +0700

    refactor: Update ShopImageSlider component to add links to individual shop pages

--- src/components/LandingPage/ShopImageSlider.tsx
+++ src/components/LandingPage/ShopImageSlider.tsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 import ShopCard from "@/components/LandingPage/ShopCard";
 import { useFetchStores } from "@/hooks/useFetchStores";
+import { Link } from "react-router-dom";
 
 const ImageSlider: React.FC = () => {
   const { data: stores } = useFetchStores();
@@ -17,38 +18,32 @@ const ImageSlider: React.FC = () => {
     cssEase: "linear",
   };
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="flex justify-center items-center h-full">
-  //       <span className="loading loading-ring loading-lg text-primary"></span>
-  //     </div>
-  //   );
-  // }
-
   return (
-    <div className="p-2">
-      <Slider {...settings}>
-        {stores?.map((store) => (
-          <div key={store.store_id}>
-            <ShopCard
-              id={store.store_id}
-              image={store.image_url || ""}
-              title={store.shop_name}
-              reservationStatus={
-                store.curr_seats < store.max_seats
-                  ? "can reserve"
-                  : "cannot reserve"
-              }
-              rating={store.rating}
-              maxSeats={store.max_seats}
-              currSeats={store.curr_seats}
-              description={store.description || ""}
-              isFavorite={store.is_favorite}
-            />
-          </div>
-        ))}
-      </Slider>
-    </div>
+    <Slider {...settings}>
+      {stores?.map((store) => (
+        <Link
+          to={`/shop/${store.store_id}`}
+          className="p-2"
+          key={store.store_id}
+        >
+          <ShopCard
+            id={store.store_id}
+            image={store.image_url || ""}
+            title={store.shop_name}
+            reservationStatus={
+              store.curr_seats < store.max_seats
+                ? "can reserve"
+                : "cannot reserve"
+            }
+            rating={store.rating}
+            maxSeats={store.max_seats}
+            currSeats={store.curr_seats}
+            description={store.description || ""}
+            isFavorite={store.is_favorite}
+          />
+        </Link>
+      ))}
+    </Slider>
   );
 };
 
