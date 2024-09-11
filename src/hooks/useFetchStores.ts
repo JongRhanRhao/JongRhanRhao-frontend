61ed4f39bcb21d0b6e1c1afdcc815b63a3c8b7d0
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 20:08:22 +0700

    refactor: Update useFetchStores hook to disable automatic refetching

--- src/hooks/useFetchStores.ts
+++ src/hooks/useFetchStores.ts
@@ -23,8 +23,10 @@ export interface Store {
 
 export const useFetchStores = () => {
   return useQuery<Store[]>({
-    queryFn: () =>
+    queryFn: async () =>
       fetch(`${SERVER_URL}/stores/api/stores`).then((res) => res.json()),
     queryKey: ["stores"],
+    refetchInterval: false,
+    refetchOnWindowFocus: true,
   });
 };
