2e854ec79a3c7d37656e160fff1dc17622d23c45
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 22:47:29 +0700

    style: Adjust menu spacing in UpgradeToVIPCard component

--- src/components/shared/UpgradeToVIPCard.tsx
+++ src/components/shared/UpgradeToVIPCard.tsx
@@ -1,6 +1,6 @@
 const UpgradeToVIPCard = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex">
       <div className="p-4 bg-primary shadow-lg rounded-xl w-10/12 h-32 overflow-hidden max-w-8xl bg-pattern">
         <header className="text-secondary font-bold text-md text-left font-sans">
           Upgrade your account to VIP
