a6420cf06353d34ef31d22751b0e2333d18dd48e
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 20:06:33 +0700

    refactor: Update UpgradeToVIPCard header styles in UpgradeToVIPCard.tsx

--- src/components/shared/UpgradeToVIPCard.tsx
+++ src/components/shared/UpgradeToVIPCard.tsx
@@ -2,7 +2,7 @@ const UpgradeToVIPCard = () => {
   return (
     <div className="flex justify-center">
       <div className="p-4 bg-primary shadow-lg rounded-xl w-10/12 h-32 overflow-hidden max-w-8xl bg-pattern">
-        <header className="text-text font-bold text-md text-left font-sans">
+        <header className="text-secondary font-bold text-md text-left font-sans">
           Upgrade your account to VIP
         </header>
         <div className="flex justify-end mt-4">
