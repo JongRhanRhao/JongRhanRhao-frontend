c0af7e7eb52d0a93389b74fd4536f4de7230e55f
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 20:08:55 +0700

    refactor: Update StoreManagement page to use ReservationsManagement component and add store selection feature

--- src/pages/MenuPages/StoreManagement.tsx
+++ src/pages/MenuPages/StoreManagement.tsx
@@ -3,11 +3,13 @@ import { useNavigate } from "react-router-dom";
 
 import { useUser } from "@/hooks/useUserStore";
 import { STORE_MANAGEMENT_MENU } from "@/lib/variables";
-import ReservationsList from "@/components/StoreManagementPage/ReservationsList";
+import ReservationsManagement from "@/components/StoreManagementPage/ReservationsManagement";
 import StaffManagement from "@/components/StoreManagementPage/StaffManagement";
 import StoreStatus from "@/components/StoreManagementPage/StoreStatus";
 import StoreSelector from "@/components/StoreManagementPage/StoreSelector";
 import { FilterButton } from "@/components/shared/FilterButton";
+import { Store } from "@/hooks/useFetchStores";
+import LinkBack from "@/components/shared/LinkBack";
 
 const StoreManagement = () => {
   const { user } = useUser();
@@ -16,9 +18,15 @@ const StoreManagement = () => {
   const [selectedType, setSelectedType] = useState(
     STORE_MANAGEMENT_MENU.RESERVATIONS
   );
+  const [selectedStore, setSelectedStore] = useState<Store | null>(null);
 
   const handleTypeClick = useCallback((title: string) => {
     setSelectedType(title);
+  }, []);
+
+  const handleStoreSelect = useCallback((store: Store | null) => {
+    setSelectedStore(store);
+    setSelectedType(STORE_MANAGEMENT_MENU.RESERVATIONS);
   }, []);
 
   useEffect(() => {
@@ -29,26 +37,33 @@ const StoreManagement = () => {
 
   const renderActiveSection = () => {
     switch (selectedType) {
-      case "Reservations":
-        return <ReservationsList />;
-      case "Store":
-        return <StoreStatus />;
-      case "Staff":
-        return <StaffManagement />;
+      case STORE_MANAGEMENT_MENU.RESERVATIONS:
+        return <ReservationsManagement store={selectedStore} />;
+      case STORE_MANAGEMENT_MENU.STORE:
+        return <StoreStatus store={selectedStore} />;
+      case STORE_MANAGEMENT_MENU.STAFF:
+        return <StaffManagement store={selectedStore} />;
       default:
         return null;
     }
   };
 
   return (
-    <div className="p-4">
-      <h2 className="text-xl font-bold text-text mb-5">Store Management</h2>
-      <StoreSelector
-        userId={user?.userId?.toString() || ""}
-        className="p-1 bg-secondary font-bold text-text text-xl border-2 border-primary rounded"
-      />
-      <div className="flex flex-wrap gap-2 mb-4">
-        {["Reservations", "Store", "Staff"].map((title) => (
+    <div className="container mx-auto">
+      <LinkBack />
+      <span className="text-xl font-bold text-text mb-5">Store Management</span>
+      <p className="text-text">
+        You can control all store details from this panel.
+      </p>
+      <div className="flex items-center">
+        <StoreSelector
+          userId={user?.userId?.toString() || ""}
+          className="font-bold text-text select bg-secondary rounded"
+          onStoreSelect={handleStoreSelect}
+        />
+      </div>
+      <div className="flex flex-wrap gap-2 mb-4 mt-4">
+        {Object.values(STORE_MANAGEMENT_MENU).map((title) => (
           <FilterButton
             key={title}
             onClick={handleTypeClick}
@@ -58,7 +73,13 @@ const StoreManagement = () => {
           />
         ))}
       </div>
-      <div className="mt-2">{renderActiveSection()}</div>
+      <div className="mt-2">
+        {selectedStore ? (
+          renderActiveSection()
+        ) : (
+          <p className="text-text">Please select a store.</p>
+        )}
+      </div>
     </div>
   );
 };
