030dbe8d53cce4af3944e5cfe4bd835e1392bffc
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 22:20:59 +0700

    refactor: Update ShopCard component to add favorite functionality and improve image loading behavior

--- src/components/LandingPage/ShopCard.tsx
+++ src/components/LandingPage/ShopCard.tsx
@@ -1,6 +1,11 @@
-import { useCallback, useState } from "react";
+import axios from "axios";
+import { useCallback, useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+import { SERVER_URL } from "@/lib/variables";
+import { useUser } from "@/hooks/useUserStore";
+import { useFavoriteStatus } from "@/hooks/useFavoriteStatus";
 
 export interface ShopCardProps {
   id: string;
@@ -15,35 +20,95 @@ export interface ShopCardProps {
 }
 
 const ShopCard: React.FC<ShopCardProps> = ({
+  id: storeId,
   image,
   title,
   reservationStatus,
   rating,
   maxSeats,
   currSeats,
-  isFavorite,
 }) => {
   const isAvailable = reservationStatus === "can reserve";
   const reservationClass = `absolute top-3 text-white text-xs font-bold px-2 py-1 rounded-r-lg ${
     isAvailable ? "bg-green-500" : "bg-rose-500"
   }`;
   const seatCountClass = `mt-2 ${isAvailable ? "text-white" : "text-red-500"}`;
-  const [isFavoriteState, setIsFavoriteState] = useState(isFavorite);
   const safeRating = Math.max(0, Math.min(5, Math.floor(rating)));
 
-  const handleFavoriteClick = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const { user } = useUser();
+  const customerId = user?.userId?.toString() || "";
+
+  const { data: isFavoriteState, refetch } = useFavoriteStatus(
+    customerId,
+    storeId
+  );
+
+  const [isFavorite, setIsFavorite] = useState<boolean>(
+    isFavoriteState || false
+  );
+
+  useEffect(() => {
+    if (isFavoriteState !== undefined) {
+      setIsFavorite(isFavoriteState);
+    }
+  }, [isFavoriteState]);
+
+  const handleUnfavoriteClick = useCallback(
+    async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
       e.preventDefault();
-      setIsFavoriteState((prev) => !prev);
+      try {
+        await axios.post(`${SERVER_URL}/stores/api/favorites/remove`, {
+          customerId,
+          storeId,
+        });
+        setIsFavorite(false);
+        refetch();
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          console.error("Axios error:", err.response);
+        } else {
+          console.error("Error removing favorite:", err);
+        }
+        setIsFavorite(true);
+      }
     },
-    []
+    [customerId, storeId, refetch]
+  );
+
+  const handleFavoriteClick = useCallback(
+    async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      e.preventDefault();
+      try {
+        if (isFavorite) {
+          handleUnfavoriteClick(e);
+        } else {
+          await axios.post(`${SERVER_URL}/stores/api/favorites`, {
+            customerId,
+            storeId,
+          });
+          setIsFavorite(true);
+          refetch();
+        }
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          console.error("Axios error:", err.response);
+        } else {
+          console.error("Error adding favorite:", err);
+        }
+        setIsFavorite(false);
+      }
+    },
+    [isFavorite, handleUnfavoriteClick, customerId, storeId, refetch]
   );
 
   return (
     <div className="card bg-gray-900 rounded overflow-hidden shadow-lg w-auto m-1 flex-shrink-0 relative cursor-pointer">
       <div className="relative">
         <img
-          src={image || "/default-image.jpg"}
+          src={
+            image ||
+            "https://static.vecteezy.com/system/resources/previews/009/007/134/original/failed-to-load-page-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-vector.jpg"
+          }
           alt={title}
           className={`w-full object-cover duration-500 ease-out aspect-auto lg:h-60 md:h-80 sm:h-32`}
         />
@@ -52,16 +117,10 @@ const ShopCard: React.FC<ShopCardProps> = ({
         >
           {reservationStatus}
         </div>
-        <div
-          className={`absolute top-2 right-2 text-white text-xl hover:text-red-600 font-bold px-2 py-1 rounded ${
-            isFavoriteState ? "text-red-500" : "text-white"
-          }`}
-        >
+        <div className="absolute top-2 right-2 text-white text-xl">
           <button onClick={handleFavoriteClick}>
             <FontAwesomeIcon
-              className={`${
-                isFavoriteState ? "text-yellow-400" : "text-white"
-              }`}
+              className={`${isFavorite ? "text-yellow-400" : "text-white"}`}
               icon={faStar}
             />
           </button>
