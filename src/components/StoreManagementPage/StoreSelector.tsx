a68ccef0e20292e5b70f37f0233cf3eb1ea929dc
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 20:08:01 +0700

    refactor: Update StoreSelector component in StoreManagementPage

--- src/components/StoreManagementPage/StoreSelector.tsx
+++ src/components/StoreManagementPage/StoreSelector.tsx
@@ -1,22 +1,37 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
 import { Store } from "@/hooks/useFetchStores";
 import { SERVER_URL } from "@/lib/variables";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-const fetchStoresByUserId = async (userId: string): Promise<Store[]> => {
-  const { data } = await axios.get(
-    `${SERVER_URL}/stores/api/stores/user/${userId}`
-  );
-  return Array.isArray(data) ? data : [];
-};
 
 const StoreSelector = ({
   userId,
   className,
+  onStoreSelect,
 }: {
   userId: string;
   className: string;
+  onStoreSelect?: (store: Store | null) => void;
 }) => {
+  const [, setSelectedStore] = useState<Store | null>(null);
+
+  const handleStoreSelection = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedStoreId = event.target.value;
+    const store = stores?.find((store) => store.store_id === selectedStoreId);
+    setSelectedStore(store || null);
+    onStoreSelect && onStoreSelect(store || null);
+  };
+
+  const fetchStoresByUserId = async (userId: string): Promise<Store[]> => {
+    const { data } = await axios.get(
+      `${SERVER_URL}/stores/api/stores/user/${userId}`
+    );
+    return Array.isArray(data) ? data : [];
+  };
+
   const {
     data: stores,
     error,
@@ -30,13 +45,16 @@ const StoreSelector = ({
   if (error) return <p>Error fetching stores: {(error as Error).message}</p>;
 
   if (!Array.isArray(stores) || stores.length === 0) {
-    return <p>No stores available.</p>;
+    return <p className="mb-4">No stores available.</p>;
   }
 
   return (
     <div>
-      <h2 className="text-text font-semibold">Select your store to manage</h2>
-      <select className={`${className} mt-2 mb-2`}>
+      <select
+        className={`${className} mt-2 mb-2`}
+        onChange={handleStoreSelection}
+      >
+        <option value="">Select a store</option>
         {stores.map((store: Store) => (
           <option key={store.store_id} value={store.store_id}>
             {store.shop_name}
