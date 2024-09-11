69461229a85eb040233d3f36e549026e9c99807d
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 22:21:56 +0700

    feat: Add refetchOnWindowFocus option to useFetchFavoriteStore hook

--- src/hooks/useFetchFavoriteStore.ts
+++ src/hooks/useFetchFavoriteStore.ts
@@ -14,5 +14,6 @@ export const useFetchFavoriteStore = (customerId: string) => {
         (res) => res.json()
       ),
     queryKey: ["favorites", customerId],
+    refetchOnWindowFocus: true,
   });
 };
