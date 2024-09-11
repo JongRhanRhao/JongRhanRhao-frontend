b1b92765710adff5b56d150dc8ea3ed5bdeb166a
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 20:08:06 +0700

    refactor: Update StoreStatus component in StoreManagementPage

--- src/components/StoreManagementPage/StoreStatus.tsx
+++ src/components/StoreManagementPage/StoreStatus.tsx
@@ -1,31 +1,171 @@
-import { STORE_STATUS } from "@/lib/variables";
+import { useState } from "react";
 import Select from "react-select";
+import axios from "axios";
 
-const StoreStatus = () => {
+import { Store } from "@/hooks/useFetchStores";
+import {
+  SERVER_URL,
+  STORE_STATUS,
+  STORE_TYPE_FOR_SELECTOR,
+} from "@/lib/variables";
+import { FilterButton } from "../shared/FilterButton";
+import toast from "react-hot-toast";
+// TODO: Fix show store default status
+const StoreStatus = ({ store }: { store: Store | null }) => {
+  const splitOldTime = store?.open_timebooking.split(" - ");
+  const storeId = store?.store_id;
+  const [status, setStatus] = useState(store?.status);
+  const [storeType, setStoreType] = useState(store?.type);
+  const [currSeat, setCurrSeat] = useState(store?.curr_seats);
+  const [maxSeat, setMaxSeat] = useState(store?.max_seats);
+  const [openingTime, setOpeningTime] = useState(
+    splitOldTime ? splitOldTime[0] || "" : ""
+  );
+  const [description, setDescription] = useState(store?.description);
+  const [closingTime, setClosingTime] = useState(
+    splitOldTime ? splitOldTime[1] || "" : ""
+  );
+
+  if (!store) {
+    return <p>No store selected.</p>;
+  }
+
+  const handleStatusChange = async () => {
+    try {
+      await axios.put(`${SERVER_URL}/stores/api/stores/${storeId}`, {
+        shopName: store.shop_name,
+        openTimeBooking: newOpeningTime,
+        cancelReserve: store.cancel_reserve,
+        ownerId: store.owner_id,
+        staffId: store.staff_id,
+        address: store.address,
+        status: status || store.status,
+        maxSeats: store.max_seats,
+        currSeats: store.curr_seats,
+        isPopular: store.is_popular,
+        type: storeType || store.type,
+        description: description || store.description,
+        imageUrl: store.image_url,
+        rating: store.rating,
+      });
+    } catch (error) {
+      console.error("Error updating store:", error);
+      throw error;
+    }
+  };
+
+  const updateStoreStatus = () => {
+    toast.promise(handleStatusChange(), {
+      loading: "Updating store...",
+      success: "Store updated successfully!",
+      error: "Error updating, please try again.",
+    });
+  };
+
+  const isTimeChanged = openingTime !== "" || closingTime !== "";
+
+  const newOpeningTime = isTimeChanged
+    ? openingTime + " - " + closingTime
+    : store.open_timebooking;
+
   return (
-    <div className="space-y-2 space-x-2 text-text font-bold">
-      <div className="text-base">Store Status:</div>
-      <Select options={STORE_STATUS} className="text-secondary w-fit" />
-      <button className="btn btn-sm uppercase">Update</button>
-      <div className="text-base">Opening Hours:</div>
-      <input
-        type="time"
-        id="appt"
-        name="appt"
-        step={1800}
-        className="p-1"
-        required
+    <div className="text-text flex flex-col container">
+      <div className="space-y-3">
+        <div className="text-base">Store Status:</div>
+        {/* <Select
+          options={STORE_STATUS}
+          className="text-secondary w-fit"
+          value={STORE_STATUS.find((option) => option.value === store.status)}
+          onChange={(selectedOption) =>
+            setStatus(selectedOption?.value?.toString() || "")
+          }
+        /> */}
+        {STORE_STATUS.map((option) => (
+          <FilterButton
+            key={option.value}
+            title={option.label}
+            selectedTitle={status}
+            onClick={() => setStatus(option.value)}
+            className="flex-grow sm:flex-grow-0 border border-primary ml-1"
+          />
+        ))}
+        <div className="collapse collapse-arrow bg-secondary w-full">
+          <input type="checkbox" />
+          <div className="collapse-title">Description:</div>
+          <div className="collapse-content">
+            <textarea
+              className="input bg-secondary textarea textarea-bordered h-auto w-full"
+              value={description || ""}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow bg-secondary w-fit">
+          <input type="checkbox" />
+          <div className="collapse-title">Seat slots:</div>
+          <div className="collapse-content">
+            <input
+              type="number"
+              value={currSeat}
+              className="input w-14 bg-secondary"
+              onChange={(e) => setCurrSeat(parseInt(e.target.value))}
+              max={maxSeat}
+            />
+            <span className="text-center m-2">/</span>
+            <input
+              type="number"
+              value={maxSeat}
+              className="input w-14 bg-secondary"
+              onChange={(e) => setMaxSeat(parseInt(e.target.value))}
+            />
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-secondary w-fit">
+          <input type="checkbox" />
+          <div className="text-base collapse-title">Opening Hours:</div>
+          <div className="collapse-content">
+            <input
+              type="time"
+              id="appt"
+              value={openingTime}
+              onChange={(e) => setOpeningTime(e.target.value)}
+              name="appt"
+              step={1800}
+              className="p-1 input bg-secondary"
+              required
+            />
+            <span className="text-center m-2">to</span>
+            <input
+              type="time"
+              id="appt"
+              value={closingTime}
+              onChange={(e) => setClosingTime(e.target.value)}
+              name="appt"
+              step={1800}
+              required
+              className="p-1 input bg-secondary"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="text-base mt-1">Store Type:</div>
+      <Select
+        options={STORE_TYPE_FOR_SELECTOR}
+        className="text-secondary w-fit mt-1"
+        value={STORE_TYPE_FOR_SELECTOR.find(
+          (option) => option.value === store.type
+        )}
+        onChange={(selectedOption) =>
+          setStoreType(selectedOption?.value?.toString() || "")
+        }
       />
-      <span className="text-center">to</span>
-      <input
-        type="time"
-        id="appt"
-        name="appt"
-        step={1800}
-        required
-        className="p-1"
-      />
-      <button className="btn btn-sm uppercase">Update</button>
+      <button
+        className="btn btn-sm bg-primary text-secondary uppercase sm:w-fit mt-4"
+        onClick={updateStoreStatus}
+      >
+        Update
+      </button>
     </div>
   );
 };
