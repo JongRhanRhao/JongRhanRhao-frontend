ae8ad84333404aaa36ed759a3d2722a2b3e4a575
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-11 20:05:22 +0700

    refactor: Add toast notifications and React Query Devtools

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        toastOptions={{
+          duration: 5000,
+          style: {
+            background: "#121212",
+            padding: "16px",
+            accentColor: "#89fc00",
+            color: "#e7ecef",
+          },
+          iconTheme: {
+            primary: "#89fc00",
+            secondary: "#121212",
+          },
+        }}
+      />
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
