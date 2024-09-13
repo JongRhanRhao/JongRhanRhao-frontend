b44dde0180f80ae42c34a5c4ce6f34296aa99352
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-14 03:03:02 +0700

    refactor: Add i18n initialization to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import "@/helper/i18n";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
