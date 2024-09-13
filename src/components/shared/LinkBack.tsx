fd4b93c4e70a131b0702d8f58cf04fbb208718ec
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-14 03:03:21 +0700

    refactor: Add i18n translation to LinkBack component

--- src/components/shared/LinkBack.tsx
+++ src/components/shared/LinkBack.tsx
@@ -1,13 +1,15 @@
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 const LinkBack = () => {
+  const { t } = useTranslation();
   return (
     <Link to="/">
       <div className="flex items-center py-2 mb-4 text-xl font-bold text-primary">
         <FontAwesomeIcon icon={faAngleLeft} />
-        <span className="ml-2">Back</span>
+        <span className="ml-2">{t("back")}</span>
       </div>
     </Link>
   );
