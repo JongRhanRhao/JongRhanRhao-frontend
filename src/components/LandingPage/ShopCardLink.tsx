48beb1b35423d4f7bfd8bb59e6997779ad57acfa
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-14 03:19:49 +0700

    refactor: Update i18n usage in ShopCardLink component

--- src/components/LandingPage/ShopCardLink.tsx
+++ src/components/LandingPage/ShopCardLink.tsx
@@ -4,22 +4,26 @@ import { Store } from "@/hooks/useFetchStores";
 import ShopCard from "@/components/LandingPage/ShopCard";
 import { Link } from "react-router-dom";
 import { STORE_AVAILABILITY_STATUS } from "@/lib/variables";
+import { useTranslation } from "react-i18next";
 
-export const ShopCardLink = React.memo(({ store }: { store: Store }) => (
-  <Link to={`/shop/${store.store_id}`} className="no-underline">
-    <ShopCard
-      id={store.store_id}
-      image={store.image_url || ""}
-      title={store.shop_name}
-      reservationStatus={
-        store.curr_seats < store.max_seats
-          ? STORE_AVAILABILITY_STATUS.AVAILABLE
-          : STORE_AVAILABILITY_STATUS.UNAVAILABLE
-      }
-      rating={store.rating}
-      maxSeats={store.max_seats}
-      currSeats={store.curr_seats}
-      description={store.description || ""}
-    />
-  </Link>
-));
+export const ShopCardLink = React.memo(({ store }: { store: Store }) => {
+  const { t } = useTranslation();
+  return (
+    <Link to={`/shop/${store.store_id}`} className="no-underline">
+      <ShopCard
+        id={store.store_id}
+        image={store.image_url || ""}
+        title={store.shop_name}
+        reservationStatus={
+          store.curr_seats < store.max_seats
+            ? t(STORE_AVAILABILITY_STATUS.AVAILABLE)
+            : t(STORE_AVAILABILITY_STATUS.UNAVAILABLE)
+        }
+        rating={store.rating}
+        maxSeats={store.max_seats}
+        currSeats={store.curr_seats}
+        description={store.description || ""}
+      />
+    </Link>
+  );
+});
