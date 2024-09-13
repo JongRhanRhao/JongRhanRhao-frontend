589c01a365aa3d66f4c246708543d4112686f6d3
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-13 21:16:44 +0700

    refactor: Update ShopCardLink component with store availability status constants

--- src/components/LandingPage/ShopCardLink.tsx
+++ src/components/LandingPage/ShopCardLink.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Store } from "@/hooks/useFetchStores";
 import ShopCard from "@/components/LandingPage/ShopCard";
 import { Link } from "react-router-dom";
+import { STORE_AVAILABILITY_STATUS } from "@/lib/variables";
 
 export const ShopCardLink = React.memo(({ store }: { store: Store }) => (
   <Link to={`/shop/${store.store_id}`} className="no-underline">
@@ -11,7 +12,9 @@ export const ShopCardLink = React.memo(({ store }: { store: Store }) => (
       image={store.image_url || ""}
       title={store.shop_name}
       reservationStatus={
-        store.curr_seats < store.max_seats ? "can reserve" : "cannot reserve"
+        store.curr_seats < store.max_seats
+          ? STORE_AVAILABILITY_STATUS.AVAILABLE
+          : STORE_AVAILABILITY_STATUS.UNAVAILABLE
       }
       rating={store.rating}
       maxSeats={store.max_seats}
