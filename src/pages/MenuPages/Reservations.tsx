438100a91e34d074fbed2d359f81cf3089d09119
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-16 23:44:10 +0700

    refactor: Update table layout and styles in Reservations component

--- src/pages/MenuPages/Reservations.tsx
+++ src/pages/MenuPages/Reservations.tsx
@@ -158,6 +158,7 @@ const Reservations = () => {
       <div className="mb-4 text-2xl font-bold text-text">
         {t("reservation")}
       </div>
+
       <div className="my-4">
         <div className="space-x-1 space-y-1">
           <FilterButton
@@ -186,93 +187,97 @@ const Reservations = () => {
           />
         </div>
       </div>
-      <table className="table w-full table-fixed">
-        <thead>
-          <tr className="bg-secondary">
-            <th className="w-1/4 px-6 py-4 font-bold text-left uppercase text-text">
-              {t("id_reservation")}
-            </th>
-            <th className="w-1/4 px-6 py-4 font-bold text-left uppercase text-text">
-              {t("shop_name")}
-            </th>
-            <th className="w-1/4 px-6 py-4 font-bold text-left uppercase text-text">
-              {t("dateNtime")}
-            </th>
-            <th className="w-1/4 px-6 py-4 font-bold text-left uppercase text-text">
-              {t("status")}
-            </th>
-            <th className="w-1/4 font-bold text-left uppercase text-text">
-              {t("action")}
-            </th>
-          </tr>
-        </thead>
-        <tbody className="bg-secondary text-text">
-          {Array.isArray(filteredReservationsByStatus(selectedStatus)) &&
-            filteredReservationsByStatus(selectedStatus)?.map((reservation) => {
-              let statusColorClass = "";
-              switch (reservation.reservation_status) {
-                case RESERVATION_STATUS.PENDING:
-                  statusColorClass =
-                    "bg-yellow-500/30 text-yellow-500 border-yellow-500";
-                  break;
-                case RESERVATION_STATUS.CONFIRMED:
-                  statusColorClass =
-                    "bg-primary/30 text-primary border-primary";
-                  break;
-                case RESERVATION_STATUS.CANCELLED:
-                  statusColorClass =
-                    "bg-red-500/30 text-red-500 border-red-500";
-                  break;
-                default:
-                  statusColorClass =
-                    "bg-yellow-500/30 text-yellow-500 border-yellow-500";
-              }
-              return (
-                <tr key={reservation.reservation_id}>
-                  <td className="px-6 py-4 border-b border-neutral-500">
-                    {reservation.reservation_id}
-                  </td>
-                  <td className="px-6 py-4 truncate border-b border-neutral-500">
-                    {reservation.shop_name}
-                  </td>
-                  <td className="px-6 py-4 border-b border-neutral-500">
-                    {format(new Date(reservation.reservation_date), "PPP")},{" "}
-                    {reservation.reservation_time}
-                  </td>
-                  <td className="px-6 py-4 border-b border-neutral-500">
-                    <span
-                      className={`${isPending(
-                        reservation.reservation_status
-                      )} px-2 py-1 text-sm rounded-full uppercase ${statusColorClass}`}
-                    >
-                      {reservation.reservation_status}
-                    </span>
-                  </td>
-                  <td className="border-b border-neutral-500">
-                    <button
-                      onClick={() => {
-                        updateCancelReserv(reservation.reservation_id);
-                      }}
-                      className={`mr-2 btn btn-outline btn-xs text-rose-500 hover:bg-rose-500/70 hover:border-rose-500`}
-                      disabled={
-                        isCancelled(reservation.reservation_status) ||
-                        isConfirmed(reservation.reservation_status)
-                      }
-                    >
-                      <FontAwesomeIcon icon={faX} />
-                    </button>
-                    <a
-                      href={`/shop/${reservation.shop_id}`}
-                      className="underline text-text/75"
-                    >
-                      <FontAwesomeIcon icon={faShop} />
-                    </a>
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
+      <div className="overflow-x-auto">
+        <table className="table w-full min-w-full table-fixed">
+          <thead>
+            <tr className="bg-secondary">
+              <th className="w-1/4 px-6 py-4 font-bold text-left uppercase text-text">
+                {t("id_reservation")}
+              </th>
+              <th className="w-1/4 px-6 py-4 font-bold text-left uppercase text-text hidden md:table-cell">
+                {t("shop_name")}
+              </th>
+              <th className="w-1/4 px-6 py-4 font-bold text-left uppercase text-text hidden sm:table-cell">
+                {t("dateNtime")}
+              </th>
+              <th className="w-1/4 px-6 py-4 font-bold text-left uppercase text-text">
+                {t("status")}
+              </th>
+              <th className="w-1/4 font-bold text-left uppercase text-text">
+                {t("action")}
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-secondary text-text">
+            {Array.isArray(filteredReservationsByStatus(selectedStatus)) &&
+              filteredReservationsByStatus(selectedStatus).map(
+                (reservation) => {
+                  let statusColorClass = "";
+                  switch (reservation.reservation_status) {
+                    case RESERVATION_STATUS.PENDING:
+                      statusColorClass =
+                        "bg-yellow-500/30 text-yellow-500 border-yellow-500";
+                      break;
+                    case RESERVATION_STATUS.CONFIRMED:
+                      statusColorClass =
+                        "bg-primary/30 text-primary border-primary";
+                      break;
+                    case RESERVATION_STATUS.CANCELLED:
+                      statusColorClass =
+                        "bg-red-500/30 text-red-500 border-red-500";
+                      break;
+                    default:
+                      statusColorClass =
+                        "bg-yellow-500/30 text-yellow-500 border-yellow-500";
+                  }
+                  return (
+                    <tr key={reservation.reservation_id}>
+                      <td className="px-6 py-4 border-b border-neutral-500">
+                        {reservation.reservation_id}
+                      </td>
+                      <td className="px-6 py-4 truncate border-b border-neutral-500 hidden md:table-cell">
+                        {reservation.shop_name}
+                      </td>
+                      <td className="px-6 py-4 border-b border-neutral-500 hidden sm:table-cell">
+                        {format(new Date(reservation.reservation_date), "PPP")},{" "}
+                        {reservation.reservation_time}
+                      </td>
+                      <td className="px-6 py-4 border-b border-neutral-500">
+                        <span
+                          className={`${isPending(
+                            reservation.reservation_status
+                          )} px-2 py-1 text-sm rounded-full uppercase ${statusColorClass}`}
+                        >
+                          {t(reservation.reservation_status)}
+                        </span>
+                      </td>
+                      <td className="border-b border-neutral-500">
+                        <button
+                          onClick={() => {
+                            updateCancelReserv(reservation.reservation_id);
+                          }}
+                          className="mr-2 btn btn-outline btn-xs text-rose-500 hover:bg-rose-500/70 hover:border-rose-500"
+                          disabled={
+                            isCancelled(reservation.reservation_status) ||
+                            isConfirmed(reservation.reservation_status)
+                          }
+                        >
+                          <FontAwesomeIcon icon={faX} />
+                        </button>
+                        <a
+                          href={`/shop/${reservation.shop_id}`}
+                          className="underline text-text/75"
+                        >
+                          <FontAwesomeIcon icon={faShop} />
+                        </a>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
