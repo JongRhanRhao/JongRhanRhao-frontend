b0cc65d484ada3843e04613a38b33c492b6abde1
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-16 18:20:38 +0700

    refactor: Update ShopDescription component
    - Add faMapLocationDot icon to display a map location link
    - Add Facebook link icon to redirect to the store's Facebook page
    - Update styling for the rating and link icons

--- src/components/ShopDescriptionPage/ShopDescription.tsx
+++ src/components/ShopDescriptionPage/ShopDescription.tsx
@@ -7,6 +7,7 @@ import {
   faClock,
   faInfoCircle,
   faLocationDot,
+  faMapLocationDot,
   faStar,
 } from "@fortawesome/free-solid-svg-icons";
 import ImageGallery from "react-image-gallery";
@@ -168,7 +169,7 @@ const ShopDescription: FC<ShopDescriptionProps> = ({ onItemClick }) => {
                 ) : (
                   <span>All</span>
                 )}
-                <div className="mt-1">
+                <div className="flex items-center mt-1">
                   {[...Array(safeRating)].map((_, i) => (
                     <FontAwesomeIcon
                       key={i}
@@ -179,6 +180,45 @@ const ShopDescription: FC<ShopDescriptionProps> = ({ onItemClick }) => {
                   <span className="ml-2 text-lg font-medium">
                     ({stores.rating})
                   </span>
+                  <div className="flex ml-2 text-text">
+                    <a
+                      href={stores.google_map_link || undefined}
+                      target="_blank"
+                    >
+                      <FontAwesomeIcon icon={faMapLocationDot} />
+                    </a>
+                    <a href={stores.facebook_link || undefined}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        x="0px"
+                        y="0px"
+                        width="100"
+                        height="100"
+                        viewBox="0 0 48 48"
+                        className="w-6 h-6 ml-2"
+                      >
+                        <linearGradient
+                          id="Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1"
+                          x1="9.993"
+                          x2="40.615"
+                          y1="9.993"
+                          y2="40.615"
+                          gradientUnits="userSpaceOnUse"
+                        >
+                          <stop offset="0" stop-color="#2aa4f4"></stop>
+                          <stop offset="1" stop-color="#007ad9"></stop>
+                        </linearGradient>
+                        <path
+                          fill="url(#Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1)"
+                          d="M24,4C12.954,4,4,12.954,4,24s8.954,20,20,20s20-8.954,20-20S35.046,4,24,4z"
+                        ></path>
+                        <path
+                          fill="#fff"
+                          d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46 c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.452 C21.988,43.9,22.981,44,24,44c0.921,0,1.82-0.084,2.707-0.204V29.301z"
+                        ></path>
+                      </svg>
+                    </a>
+                  </div>
                 </div>
                 <p className="my-4 text-lg break-words text-text">
                   {stores.description}
