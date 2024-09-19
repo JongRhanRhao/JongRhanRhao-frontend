199368866ba7ac8dd3340184a9da038356ba800c
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-19 18:33:46 +0700

    refactor: Update ShopDescription component styles and structure

--- src/components/ShopDescriptionPage/ShopDescription.tsx
+++ src/components/ShopDescriptionPage/ShopDescription.tsx
@@ -7,10 +7,10 @@ import {
   faClock,
   faInfoCircle,
   faLocationDot,
-  // faMapLocationDot,
+  faMapLocationDot,
   faStar,
 } from "@fortawesome/free-solid-svg-icons";
-import ImageGallery from "react-image-gallery";
+// import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import { useTranslation } from "react-i18next";
 
@@ -23,7 +23,7 @@ import { Store } from "@/hooks/useFetchStores";
 import SmallScreenNavMenu from "@/components/shared/SmallScreenNavMenu";
 import LinkBack from "@/components/shared/LinkBack";
 import FavoriteButton from "@/components/shared/FavoriteButton";
-import { useFetchStoreImages } from "@/hooks/useFetchStoreImages";
+// import { useFetchStoreImages } from "@/hooks/useFetchStoreImages";
 import { socket } from "@/socket";
 
 interface ShopDescriptionProps {
@@ -147,13 +147,14 @@ const ShopDescription: FC<ShopDescriptionProps> = ({ onItemClick }) => {
                 className="object-cover w-full rounded-xl h-80 md:w-1/2"
                 alt="shopImage"
               />
+              {/*  */}
               <div className="w-full h-auto p-5 mt-5 shadow-md rounded-xl md:ml-8 md:mt-0">
                 <div className="flex items-baseline space-x-2">
                   <div className="mb-2 text-4xl font-bold uppercase text-text">
                     {t(stores.shop_name)}
                   </div>
                   <FavoriteButton
-                    className="p-1 text-2xl rounded bg-secondary"
+                    className="p-1.5 text-2xl bg-secondary rounded-full"
                     storeId={storeId ? storeId : ""}
                   />
                 </div>
@@ -181,44 +182,47 @@ const ShopDescription: FC<ShopDescriptionProps> = ({ onItemClick }) => {
                     ({stores.rating})
                   </span>
                   <div className="flex ml-2 text-text">
-                    {/* <a
+                    <a
                       href={stores.google_map_link || undefined}
                       target="_blank"
                     >
-                      <FontAwesomeIcon icon={faMapLocationDot} />
-                    </a> */}
-                    <a href={stores.facebook_link || undefined}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        x="0px"
-                        y="0px"
-                        width="100"
-                        height="100"
-                        viewBox="0 0 48 48"
-                        className="w-6 h-6 ml-2"
-                      >
-                        <linearGradient
-                          id="Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1"
-                          x1="9.993"
-                          x2="40.615"
-                          y1="9.993"
-                          y2="40.615"
-                          gradientUnits="userSpaceOnUse"
-                        >
-                          <stop offset="0" stopColor="#2aa4f4"></stop>
-                          <stop offset="1" stopColor="#007ad9"></stop>
-                        </linearGradient>
-                        <path
-                          fill="url(#Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1)"
-                          d="M24,4C12.954,4,4,12.954,4,24s8.954,20,20,20s20-8.954,20-20S35.046,4,24,4z"
-                        ></path>
-                        <path
-                          fill="#fff"
-                          d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46 c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.452 C21.988,43.9,22.981,44,24,44c0.921,0,1.82-0.084,2.707-0.204V29.301z"
-                        ></path>
-                      </svg>
+                      <FontAwesomeIcon
+                        className="text-text/70"
+                        icon={faMapLocationDot}
+                      />
                     </a>
                   </div>
+                  <a href={stores.facebook_link || undefined}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px"
+                      width="100"
+                      height="100"
+                      viewBox="0 0 48 48"
+                      className="w-6 h-6 ml-2"
+                    >
+                      <linearGradient
+                        id="Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1"
+                        x1="9.993"
+                        x2="40.615"
+                        y1="9.993"
+                        y2="40.615"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop offset="0" stopColor="#2aa4f4"></stop>
+                        <stop offset="1" stopColor="#007ad9"></stop>
+                      </linearGradient>
+                      <path
+                        fill="url(#Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1)"
+                        d="M24,4C12.954,4,4,12.954,4,24s8.954,20,20,20s20-8.954,20-20S35.046,4,24,4z"
+                      ></path>
+                      <path
+                        fill="#fff"
+                        d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46 c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.452 C21.988,43.9,22.981,44,24,44c0.921,0,1.82-0.084,2.707-0.204V29.301z"
+                      ></path>
+                    </svg>
+                  </a>
                 </div>
                 <p className="my-4 text-lg break-words text-text">
                   {stores.description}
@@ -268,7 +272,7 @@ const ShopDescription: FC<ShopDescriptionProps> = ({ onItemClick }) => {
                 </div>
               </div>
             </div>
-            <div className="mt-10">
+            <div className="mt-2">
               <CommentSection />
             </div>
           </main>
