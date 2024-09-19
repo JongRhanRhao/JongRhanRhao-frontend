92ac18a766c7a70d2113eda386a973250143cf37
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-19 18:32:49 +0700

    refactor: Update CommentCard component styles and structure

--- src/components/ShopDescriptionPage/CommentCard.tsx
+++ src/components/ShopDescriptionPage/CommentCard.tsx
@@ -1,51 +1,48 @@
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { format } from "date-fns";
+import { useTranslation } from "react-i18next";
+
 const CommentCard = ({
   name,
   avatar,
   date,
-  likes,
   comment,
-  replies,
+  rating,
 }: {
   name: string;
   avatar: string;
   date: string;
-  likes: number;
   comment: string;
-  replies: number;
+  rating: number;
 }) => {
+  const { t } = useTranslation();
   return (
-    <div className="max-w-lg px-6 py-4 rounded-lg">
+    <div className="max-w-sm px-6 py-4 shadow-lg bg-secondary rounded-xl">
       <div className="flex items-center mb-6">
-        <img
-          src={avatar}
-          alt="Avatar"
-          className="w-12 h-12 mr-4 rounded-full"
-        />
+        <div className="avatar">
+          <div className="w-16 mr-4 rounded-full">
+            <img src={avatar} />
+          </div>
+        </div>
         <div>
-          <div className="text-lg font-medium text-white">{name}</div>
-          <div className="text-gray-500">{date}</div>
+          <div className="font-medium text text-text">{name}</div>
         </div>
       </div>
-      <p className="mb-6 text-lg leading-relaxed text-white">{comment}</p>
-      <div className="flex items-center justify-between">
-        <div className="flex justify-between">
-          <a href="#" className="mr-4 text-gray-500 hover:text-primary">
-            <i className="far fa-thumbs-up" /> {likes}
-            <i className="far fa-thumbs-up" /> Like
-          </a>
-          <a href="#" className="text-gray-500 hover:text-primary">
-            <i className="far fa-comment-alt" /> {replies}
-            <i className="far fa-comment-alt" /> Reply
-          </a>
+      <p className="mb-6 leading-relaxed text-text">{comment}</p>
+      <div className="flex items-center justify-between gap-8">
+        <div className="text-text/50">
+          {Array.from({ length: rating }).map((_, index) => (
+            <FontAwesomeIcon
+              key={index}
+              icon={faStar}
+              className="text-yellow-500"
+            />
+          ))}
         </div>
-        {/* <div className="flex items-center">
-            <a href="#" className="mr-4 text-gray-500 hover:text-primary">
-              <i className="far fa-flag" /> Report
-            </a>
-            <a href="#" className="text-gray-500 hover:text-primary">
-              <i className="far fa-share-square" /> Share
-            </a>
-          </div> */}
+        <div className="text-sm text-text/50">
+          {t("Reviewed on")} {format(date, "dd/MM/yyyy")}
+        </div>
       </div>
     </div>
   );
