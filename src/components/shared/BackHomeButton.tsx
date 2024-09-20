72b64bcce108acd74b30387dae07bf3f54b04900
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-20 14:57:32 +0700

    refactor: Update hover styles for BackHomeButton component

--- src/components/shared/BackHomeButton.tsx
+++ src/components/shared/BackHomeButton.tsx
@@ -2,7 +2,7 @@ const BackHomeButton = ({ className }: { className?: string }) => {
   return (
     <a
       href="/"
-      className={`btn btn-outline border-primary text-primary ${className}`}
+      className={`btn btn-outline border-primary text-primary hover:bg-primary hover:text-secondary hover:border-none ${className}`}
     >
       Go Home
     </a>
