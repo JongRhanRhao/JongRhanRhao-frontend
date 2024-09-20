a8b8b78f2d91be0f12a99ca99ddeb421b6915441
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-20 16:17:16 +0700

    refactor: Update NotFoundPage component layout and text alignment

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,11 +1,11 @@
 const NotFoundPage = () => {
   return (
     <>
-      <div className="min-h-screen text-center hero text-primary">
+      <div className="text-center  hero text-primary">
         <div className="flex-col hero-content">
           <h1 className="font-bold text-9xl">404</h1>
-          <p className="mt-4 text-2xl font-medium">Oops! Page not found</p>
-          <p className="mt-4 mb-8">
+          <p className="text-2xl font-medium mt-">Oops! Page not found</p>
+          <p className="mb-8">
             The page you're looking for doesn't exist or has been moved.
           </p>
           <a href="/" className="btn btn-outline border-primary text-primary">
