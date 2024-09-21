ad84a72d79b647a8c0f927e62051de460ba40fe5
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-21 11:29:55 +0700

    style: Update style

--- src/components/LandingPage/ImageSliderBackground.tsx
+++ src/components/LandingPage/ImageSliderBackground.tsx
@@ -20,7 +20,7 @@ const ImageSliderBackground: React.FC<ImageSliderBackgroundProps> = ({
 
   return (
     <div
-      className="absolute top-0 left-0 right-0 z-0 bg-cover bg-center blur-lg transform"
+      className="absolute top-0 left-0 right-0 z-0 bg-cover bg-center blur-lg duration-500 ease-in transform"
       style={{
         height,
         backgroundImage: `url(${currentStore.image_url})`,
