e7ac43d2ea62944a5e6990e5bf98e0f2ade75ace
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-22 14:37:17 +0700

    feat: Add FeedbackPage component for customer feedback

---
+++ src/pages/FeedbackPage.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useTranslation } from "react-i18next";
+
+import { CUSTOM_BUTTON_CLASS } from "@/lib/variables";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type FeedbackProps = {
+  name?: string;
+  email?: string;
+  message?: string;
+  onClick?: () => void;
+};
+
+const FeedbackPage: React.FC<FeedbackProps> = () => {
+  const titleClass = "mt-4 text-text font-semibold text-sm";
+  const inputClass =
+    "mt-2 input text-sm bg-secondary text-text placeholder-text/50 w-full";
+
+  const { t } = useTranslation();
+
+  const feedbackSchema = z.object({
+    name: z.string().nonempty({ message: "* Name is required" }),
+    email: z.string().email(),
+    message: z.string().nonempty({ message: "* Message is required" }),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FeedbackProps>({
+    resolver: zodResolver(feedbackSchema),
+  });
+  const onSubmit = (data: FeedbackProps) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="w-4/5 min-w-full flex mx-auto">
+      <div className="card card-body bg-secondary/30 w-1/2">
+        <div>
+          <h1 className="text-3xl text-text font-bold">
+            {t("Customer Feedback")}
+          </h1>
+          <p className="text-sm text-text/50 mt-2">
+            {t("Thank you for using our service. Please leave us a feedback.")}
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)} className="mt-10">
+            <div className={titleClass}>
+              {t("Name")} <span className="text-rose-500">*</span>
+            </div>
+            <input
+              {...register("name")}
+              type="text"
+              className={inputClass}
+              placeholder={t("Enter your full name")}
+            />
+            {errors.name && (
+              <p className="mt-1 text-sm text-red-600">{errors.name.message}</p>
+            )}
+            <div className={titleClass}>
+              {t("E-mail")} <span className="text-rose-500">*</span>
+            </div>
+            <input
+              {...register("email")}
+              type="email"
+              className={inputClass}
+              placeholder={t("Enter your email address")}
+            />
+            <div className={titleClass}>{t("Message")}</div>
+            <textarea
+              {...register("message")}
+              name="message"
+              id="message"
+              className="mt-2 textarea textarea-lg text-sm bg-secondary text-text placeholder-text/50 w-full"
+              placeholder={t("Write your message...")}
+            ></textarea>
+            {errors.message && (
+              <p className="mt-1 text-sm text-red-600">
+                {errors.message.message}
+              </p>
+            )}
+            <button
+              type="submit"
+              className={`w-fit mt-4 ${CUSTOM_BUTTON_CLASS}`}
+            >
+              {t("Submit Feedback")}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackPage;
