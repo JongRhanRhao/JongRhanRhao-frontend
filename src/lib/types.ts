c4085935c6da97a363d93ad5f1d2329ee02c13b0
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-24 18:38:48 +0700

    feat: Add birthYear field to RegisterSchema

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -15,6 +15,7 @@ export const RegisterSchema = BaseUserSchema.extend({
   confirm_password: z
     .string()
     .min(6, "* Password must be at least 6 characters long"),
+  birthYear: z.number().int().min(1900).max(new Date().getFullYear()),
 }).refine((data) => data.password === data.confirm_password, {
   message: "* Passwords do not match",
   path: ["confirm_password"],
