d53748ed5667dce273cd9fb421d6ded990f077e1
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-27 08:52:10 +0700

    refactor: Update CommentSection sliderSettings for responsive design

--- src/components/ShopDescriptionPage/CommentSection.tsx
+++ src/components/ShopDescriptionPage/CommentSection.tsx
@@ -29,6 +29,20 @@ const CommentSection = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   const reviewData: Review = {
