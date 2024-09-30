8175687a826a9e53f3759ce64f2ab4b4ba9e4bcb
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-30 18:12:02 +0700

    refactor: Update BookingCalendar component to improve date selection and localization

--- src/components/ShopDescriptionPage/BookingCalendar.tsx
+++ src/components/ShopDescriptionPage/BookingCalendar.tsx
@@ -1,26 +1,27 @@
-import i18n from "@/helper/i18n";
 import DatePicker from "react-datepicker";
 import { th } from "date-fns/locale";
+import i18n from "@/helper/i18n";
+
+interface BookingCalendarProps {
+  selectedDate: Date | null;
+  setSelectedDate: (date: Date | null) => void;
+  storeId: string;
+}
 
 const BookingCalendar = ({
   selectedDate,
   setSelectedDate,
-}: {
-  selectedDate: Date | null;
-  setSelectedDate: (date: Date | null) => void;
-}) => {
+}: BookingCalendarProps) => {
   return (
-    <>
-      <DatePicker
-        selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
-        dateFormat="d MMMM yyyy"
-        className="p-2 mt-1 rounded-xl bg-secondary"
-        minDate={new Date()}
-        maxDate={new Date(new Date().setDate(new Date().getDate() + 30))}
-        locale={i18n.language == "th" ? th : ""}
-      />
-    </>
+    <DatePicker
+      selected={selectedDate}
+      onChange={(date) => setSelectedDate(date)}
+      dateFormat="d MMMM yyyy"
+      minDate={new Date()}
+      locale={i18n.language === "th" ? th : undefined}
+      inline
+      maxDate={new Date(new Date().setMonth(new Date().getMonth() + 1))}
+    />
   );
 };
 
