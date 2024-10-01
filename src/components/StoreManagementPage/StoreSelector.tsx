8f3c743c44c47d9585998358c92dbc065146ea73
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-10-01 13:51:26 +0700

    refactor: Update StoreSelector component to handle store selection and persist selected store

--- src/components/StoreManagementPage/StoreSelector.tsx
+++ src/components/StoreManagementPage/StoreSelector.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { Store } from "@/hooks/useFetchStores";
@@ -15,18 +15,8 @@ const StoreSelector = ({
   className: string;
   onStoreSelect?: (store: Store | null) => void;
 }) => {
-  const [, setSelectedStore] = useState<Store | null>(null);
+  const [selectedStore, setSelectedStore] = useState<Store | null>(null);
   const { t } = useTranslation();
-
-  const handleStoreSelection = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const selectedStoreId = event.target.value;
-    const store = stores?.find((store) => store.store_id === selectedStoreId);
-    setSelectedStore(store || null);
-    onStoreSelect && onStoreSelect(store || null);
-    refetch();
-  };
 
   const fetchStoresByUserId = async (userId: string): Promise<Store[]> => {
     const { data } = await axios.get(
@@ -45,20 +35,53 @@ const StoreSelector = ({
     queryFn: () => fetchStoresByUserId(userId),
   });
 
+  useEffect(() => {
+    const savedStoreId = localStorage.getItem("selectedStoreId");
+    if (savedStoreId && stores) {
+      const storedStore = stores.find(
+        (store) => store.store_id === savedStoreId
+      );
+      setSelectedStore(storedStore || null);
+      onStoreSelect && onStoreSelect(storedStore || null);
+    }
+  }, [stores, onStoreSelect]);
+
+  const handleStoreSelection = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedStoreId = event.target.value;
+    const store = stores?.find((store) => store.store_id === selectedStoreId);
+
+    if (store) {
+      localStorage.setItem("selectedStoreId", selectedStoreId);
+    } else {
+      localStorage.removeItem("selectedStoreId");
+    }
+
+    setSelectedStore(store || null);
+    onStoreSelect && onStoreSelect(store || null);
+    refetch();
+  };
+
   if (isLoading) return <p>Loading stores...</p>;
   if (error) return <p>Error fetching stores: {(error as Error).message}</p>;
 
   if (!Array.isArray(stores) || stores.length === 0) {
     return (
-      <p className="p-2 border rounded text-rose-500 border-rose-500">
-        No stores available. <br />
+      <p className="p-2 text-rose-500">
+        {t("No stores available.")}
+        <br />
       </p>
     );
   }
 
   return (
     <div>
-      <select className={`${className}`} onChange={handleStoreSelection}>
+      <select
+        className={`${className}`}
+        onChange={handleStoreSelection}
+        value={selectedStore?.store_id || ""}
+      >
         <option value="">{t("selectYourStore")}</option>
         {stores.map((store: Store) => (
           <option key={store.store_id} value={store.store_id}>
