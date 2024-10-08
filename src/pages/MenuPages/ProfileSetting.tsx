202677aedd28dc3569a03d0ba4ae33c07ee31f68
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-10-08 19:00:32 +0700

    Refactor: Update ProfileSetting component

--- src/pages/MenuPages/ProfileSetting.tsx
+++ src/pages/MenuPages/ProfileSetting.tsx
@@ -6,22 +6,16 @@ import { PhoneInput } from "react-international-phone";
 
 import LinkBack from "@/components/shared/LinkBack";
 import { useUser } from "@/hooks/useUserStore";
-import {
-  // CUSTIOM_BUTTON_OUTLINE_CLASS,
-  CUSTOM_BUTTON_CLASS,
-  SERVER_URL,
-} from "@/lib/variables";
+import { CUSTOM_BUTTON_CLASS, SERVER_URL } from "@/lib/variables";
 import LoginButton from "@/components/shared/LoginButton";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import LogoutButton from "@/components/shared/LogoutButton";
 
 const ProfileSetting = () => {
-  const { user, isAuthenticated, logout } = useUser();
+  const { user, isAuthenticated } = useUser();
   const [userName, setUserName] = useState<string>(user?.userName || "");
   const [phoneNumber, setPhoneNumber] = useState<string>(
     user?.phoneNumber || ""
   );
-  // const [profilePicture, setProfilePicture] = useState<File>();
   const { t } = useTranslation();
 
   if (!isAuthenticated) {
@@ -55,53 +49,6 @@ const ProfileSetting = () => {
     });
   };
 
-  // const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const file = e.target.files?.[0];
-  //   if (file) {
-  //     setProfilePicture(file);
-  //   }
-  // };
-
-  // const handleUpload = async () => {
-  //   if (!profilePicture) {
-  //     toast.error(t("No file selected."));
-  //     return Promise.reject();
-  //   }
-
-  //   const formData = new FormData();
-  //   // formData.append("profileImage", profilePicture);
-
-  //   try {
-  //     await axios.post(
-  //       `${SERVER_URL}/users/${user?.userId}/profile-image`,
-  //       formData,
-  //       {
-  //         headers: {
-  //           "Content-Type": "multipart/form-data",
-  //         },
-  //       }
-  //     );
-  //   } catch (error) {
-  //     return Promise.reject(error);
-  //   }
-  // };
-
-  // const uploadStatus = () => {
-  //   toast.promise(handleUpload(), {
-  //     loading: t("Uploading image..."),
-  //     success: t("Image uploaded successfully!"),
-  //     error: t("Something went wrong. Please try again."),
-  //   });
-  // };
-
-  const handleLogout = async () => {
-    try {
-      await logout();
-    } catch (error) {
-      toast.error(t("Something went wrong. Please try again."));
-    }
-  };
-
   return (
     <>
       <LinkBack />
@@ -111,22 +58,8 @@ const ProfileSetting = () => {
           <div className="flex items-center gap-4">
             <div className="avatar">
               <div className="w-24 rounded-full">
-                <img src={user?.profilePicture} />
+                <img src={user?.profilePicture} alt="Profile" />
               </div>
-            </div>
-            <div className="flex flex-col ml-4 space-y-2">
-              {/* <input
-                type="file"
-                className="w-full max-w-xs file-input file-input-bordered file-input-xs bg-secondary text-text"
-                accept=".jpg,.jpeg,.png"
-                onChange={handleFileChange}
-              />
-              <button
-                className={`btn-sm w-fit ${CUSTIOM_BUTTON_OUTLINE_CLASS}`}
-                onClick={uploadStatus}
-              >
-                {t("Upload new photo")}
-              </button> */}
             </div>
           </div>
           <div className="w-full p-4 mt-6 text-text bg-secondary/50 rounded-xl">
@@ -148,11 +81,6 @@ const ProfileSetting = () => {
                 setPhoneNumber(phone);
               }}
             />
-            {/* //TODO: cnx, bkk  */}
-            {/* <select className="w-full mt-4 input input-md bg-secondary text-text">
-              <option value="th">Thailand</option>
-              <option value="en">English</option>
-            </select> */}
             <div className="card-actions">
               <button
                 onClick={userUpdateStatus}
@@ -165,13 +93,7 @@ const ProfileSetting = () => {
                 {t("Update")}
               </button>
             </div>
-            <button
-              onClick={handleLogout}
-              className="mt-2 min-w-fit btn btn-outline btn-error"
-            >
-              <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
-              {t("Logout")}
-            </button>
+            <LogoutButton />
           </div>
         </div>
       </div>
