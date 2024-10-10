d7b7bdb9a9a1754bc5d9de06a9d253752b38fc3b
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-10-10 12:02:38 +0700

    Refactor: Add isClosed flag to ShopCard component

--- src/components/LandingPage/ShopCard.tsx
+++ src/components/LandingPage/ShopCard.tsx
@@ -32,6 +32,7 @@ const ShopCard: React.FC<ShopCardProps> = ({
 }) => {
   const isAvailable = storeStatus === "Available";
   const isBusy = storeStatus === "Busy";
+  const isClosed = storeStatus === "Closed";
   const reservationClass = `absolute top-3 text-secondary text-xs font-bold px-2 py-1 rounded-r-lg ${
     isAvailable ? "bg-primary" : isBusy ? "bg-yellow-500" : "bg-rose-500"
   }`;
@@ -58,7 +59,9 @@ const ShopCard: React.FC<ShopCardProps> = ({
               "https://static.vecteezy.com/system/resources/previews/009/007/134/original/failed-to-load-page-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-vector.jpg"
             }
             alt={title}
-            className={`w-full object-cover duration-150 ease-out aspect-auto lg:h-60 md:h-80 sm:h-32 ${className}`}
+            className={`${
+              isClosed ? "grayscale" : ""
+            } w-full object-cover duration-150 ease-out aspect-auto lg:h-60 md:h-80 sm:h-32 ${className}`}
           />
         ) : (
           <div className="w-full skeleton h-60"></div>
@@ -78,27 +81,38 @@ const ShopCard: React.FC<ShopCardProps> = ({
         <div
           className={`${ImageSliderClass} absolute px-2 py-1 rounded-r bottom-1`}
         >
-          <div className="px-1 space-x-2 text-sm badge w-fit rounded-xl bg-secondary/80 text-text">
+          <div className="px-1 text-sm space-x-2 badge w-fit rounded-xl bg-secondary/80 text-text">
             <FontAwesomeIcon
               icon={faStar}
-              className="text-yellow-400 shadow-lg"
+              className={`${
+                isClosed ? "text-gray-400" : "text-yellow-400"
+              } shadow-lg`}
             />
-            <div>
+            <div className={`${isClosed ? "text-gray-400" : "text-text"}`}>
               {safeRating === 0 ? t("No Rating") : safeRating.toFixed(1)}
             </div>
           </div>
           <div
             className={`badge space-x-2 bg-secondary/80 w-fit rounded-xl px-1`}
           >
-            <FontAwesomeIcon icon={faClock} className="text-xs text-text" />
-            <div className={`text-text text-xs`}>{open_timebooking}</div>
+            <FontAwesomeIcon
+              icon={faClock}
+              className={`text-xs ${isClosed ? "text-gray-400" : "text-text"}`}
+            />
+            <div
+              className={`${isClosed ? "text-gray-400" : "text-text"} text-xs`}
+            >
+              {open_timebooking}
+            </div>
           </div>
           <br />
           {type && Array.isArray(type) ? (
             type.map((type: string, index: number) => (
               <div
                 key={index}
-                className={`badge badge-ghost bg-secondary/80 text-xs text-text mr-0.5`}
+                className={`badge badge-ghost bg-secondary text-xs ${
+                  isClosed ? "text-gray-400" : "text-text"
+                } mr-0.5`}
               >
                 {t(type)}
               </div>
