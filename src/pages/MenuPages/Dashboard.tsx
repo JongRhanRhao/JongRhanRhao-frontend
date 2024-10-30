db90392fde12c19e0b6228741e85f5a2d52f5718
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-10-30 08:57:47 +0700

    chore: remove unused code

--- src/pages/MenuPages/Dashboard.tsx
+++ src/pages/MenuPages/Dashboard.tsx
@@ -1,7 +1,6 @@
 import StoreListWithFilterFeature from "@/components/LandingPage/StoreListWithFilterFeature";
 import SynchronizedImageSlider from "@/components/LandingPage/SynchronizedImageSlider";
 
-// TODO: Add location filter and selector feature e.g. by city, by district
 const DashboardContent = () => {
   return (
     <div className="relative">
