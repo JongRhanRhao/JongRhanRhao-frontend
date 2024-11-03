c18b04cf4c256ae08c38198b567419d9c0abb453
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-11-03 18:24:16 +0700

    chore: enable withCredentials for socket connection

--- src/socket.ts
+++ src/socket.ts
@@ -1,4 +1,6 @@
 import io, { Socket } from "socket.io-client";
 import { SERVER_URL } from "@/lib/variables";
 
-export const socket: Socket = io(SERVER_URL);
+export const socket: Socket = io(SERVER_URL, {
+  withCredentials: true,
+});
